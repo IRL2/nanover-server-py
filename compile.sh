d9d377338d13fd0c7a0f34837ea7762fb67d6bba
Mike O'Connor <mikeoconnor0308@gmail.com>
2019-03-01 17:57:55 +0000

    add auto-init to compile scripts

--- compile.sh
+++ compile.sh
@@ -3,5 +3,9 @@ cd protocol
 find ./ -name *.proto -print0 | while read -d $'\0' file
 do
   echo $file
-  C:/Users/IT036394-Admin/Anaconda3/envs/narupa-protocol/python.exe -m grpc_tools.protoc -I ./ --python_out=../project/python/narupa-protocol --grpc_python_out=../project/python/narupa-protocol $file
+  python -m grpc_tools.protoc -I ./ --python_out=../project/python/narupa-protocol --grpc_python_out=../project/python/narupa-protocol $file
 done
+cd ..
+# add init files to all generated python files.
+find project/python/narupa-protocol/narupa/protocol -type d -exec touch {}/__init__.py \;
+
