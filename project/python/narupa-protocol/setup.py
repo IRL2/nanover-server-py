dd7f658f371f3b8886ae5dd4581f4012788cf6c7
Mike O'Connor <mikeoconnor0308@gmail.com>
2019-03-15 11:29:39 +0000

    add setup.py for narupa python modules

---
+++ project/python/narupa-protocol/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='Narupa',
+      version='1.0',
+      description='Narupa python framework',
+      author='Intangible Realities Lab',
+      author_email='m.oconnor@bristol.ac.uk',
+      url='https://gitlab.com/intangiblerealities/',
+      packages=find_packages(),
+     )
