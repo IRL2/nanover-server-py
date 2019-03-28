126d582373947494320ec7f30a59389a063dd44a
Jonathan Barnoud <jonathan@barnoud.net>
2019-03-28 15:21:08 +0000

    Update compile.sh for the new organisation

--- compile.sh
+++ compile.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
-echo "Checking that the requirements.txt is satisfied using pip"
-python -m pip install -r ./python-libraries/narupa-core/requirements.txt --user
 
+function announce() {
+    echo -e "\e[1;34m$1\e[0m"
+}
 
+# Stop if something goes wrong to avoid building on errors.
+# * "set -e" mean that the script stops if a commands ends with a non-zero
+#   exit code
+# * "set -u" stops if we refer to an undefined variable
+# * "set -o pipefail" considers that a series of piped command fails not only
+#   if the last command fails, but if any of them do
+set -euo pipefail
 
-cd protocol
-find ./ -name *.proto -print0 | while read -d $'\0' file
+# Ideally, users should not use "pip install --user". This tends to polute
+# the python environment and lead to unpleasant surprises where users are
+# using old versions of libraries while they think they installed a newer
+# version. Though, using "--user" is sometimes the easiest way of installing
+# libraries, so it should be an option.
+# Running "./compile.sh --user" will pip install with the --user option.
 
-
-do
-  echo $file
-  python -m grpc_tools.protoc -I ./ --python_out=../python-libraries/narupa-core --grpc_python_out=../python-libraries/narupa-core $file
+# A developper most likely want to install narupa's python packages in edit
+# mode. We add a --edit option to the script to allow this.
+user_option=""
+edit_option=""
+for option in "$@"; do
+    if [[ "$option" == "--user" ]]; then
+        user_option="--user"
+    elif [[ "$option" == "--edit" ]]; then
+        edit_option="-e"
+    fi
 done
-cd ..
-# add init files to all generated python files.
-find ./python-libraries/narupa-core/narupa/protocol -type d -exec touch {}/__init__.py \;
+# We do not want to use pip with --user if we use -e.
+narupa_user_option=${user_option}
+if [[ ! -z "${edit_option}" ]]; then
+    narupa_user_option=""
+fi
 
+announce "Installing python requirements"
+python -m pip install -r ./python-libraries/narupa-core/requirements.txt ${user_option}
+
+announce "Compiling proto files to python"
+python ./python-libraries/narupa-core/setup.py compile_proto
+
+announce "Installing the python packages"
+python -m pip install ${edit_option} ${narupa_user_option} \
+    ./python-libraries/narupa-core/
+python -m pip install ${edit_option} ${narupa_user_option} \
+    ./python-libraries/narupa-openmm/
+
+python -c "import simtk" 2>&1 > /dev/null || {
+    announce "OpenMM is not installed."
+    announce "See <http://docs.openmm.org/latest/userguide/application.html#installing-openmm>."
+}
+
+announce "Compiling proto files to C#"
 cd csharp-libraries/Narupa.Protocol
-
 dotnet build
+cd ../
