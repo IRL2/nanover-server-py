4e6d196f16d776c87e94962e34dc4b8c3203fc0a
Alex Binnie <alexjbinnie@gmail.com>
2019-04-01 20:29:38 +0100

    Refactored ASE integration into new project structure

---
+++ python-libraries/narupa-ase/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_namespace_packages
+
+setup(name='narupa-ase',
+      version='0.1.0',
+      description='ASE integration for Narupa',
+      author='Intangible Realities Lab',
+      author_email='m.oconnor@bristol.ac.uk',
+      url='https://gitlab.com/intangiblerealities/',
+      packages=find_namespace_packages('src', include='narupa.*'),
+      package_dir={'': 'src'},
+      requires=(
+            'narupa',
+            'ase',
+      ),
+     )
