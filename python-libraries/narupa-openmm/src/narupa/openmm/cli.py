107c7b6e942b4cc4eee0f756e01728d5e618254b
Jonathan Barnoud <jonathan@barnoud.net>
2019-04-08 14:35:26 +0100

    Impove command line interface for narupa.openmm

--- python-libraries/narupa-openmm/src/narupa/openmm/cli.py
+++ python-libraries/narupa-openmm/src/narupa/openmm/cli.py
@@ -1,6 +1,7 @@
 """
 Command line interface for narupa.openmm.
 """
+import textwrap
 import argparse
 from . import Runner, Server
 
@@ -11,13 +12,28 @@ def handle_user_arguments() -> argparse.Namespace:
 
     :return: The namespace of arguments read from the command line.
     """
-    parser = argparse.ArgumentParser()
+    description = textwrap.dedent("""\
+    Run an OpenMM simulation and send it to the network for Narupa.
+    """)
+    parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument('system_xml_path')
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument(
+        'simulation_xml_path',
+        help='The simulation to run in XML format.',
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help=('Display the step number, the potential energy in kJ/mol, '
+              'and the performance in ns/day.'),
+    )
     parser.add_argument('-p', '--port', default=54321)
     parser.add_argument('-a', '--address', default='[::]')
-    parser.add_argument('--no-serve', dest='do_serve', action='store_false', default=True)
+    parser.add_argument(
+        '--no-serve',
+        dest='do_serve', action='store_false', default=True,
+        help='Do not send the trajectory over the network for Narupa.',
+    )
 
     arguments = parser.parse_args()
     return arguments
@@ -31,10 +47,12 @@ def main():
 
     if arguments.do_serve:
         simulation_runner = Server.from_xml_input(
-            input_xml=arguments.system_xml_path,
+            input_xml=arguments.simulation_xml_path,
             address=arguments.address,
             port=arguments.port,
         )
+    else:
+        simulation_runner = Runner.from_xml_input(arguments.simulation_xml_path)
     simulation_runner.verbose = arguments.verbose
     simulation_runner.run()
 
