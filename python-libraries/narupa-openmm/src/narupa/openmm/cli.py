c9d6f23ee5cd2c82d7371eeacbd8f440f314c66d
Jonathan Barnoud <jonathan@barnoud.net>
2019-04-08 14:08:53 +0100

    Narupa.openmm can serve from the CLI

--- python-libraries/narupa-openmm/src/narupa/openmm/cli.py
+++ python-libraries/narupa-openmm/src/narupa/openmm/cli.py
@@ -2,7 +2,7 @@
 Command line interface for narupa.openmm.
 """
 import argparse
-from . import Runner
+from . import Runner, Server
 
 
 def handle_user_arguments() -> argparse.Namespace:
@@ -12,8 +12,12 @@ def handle_user_arguments() -> argparse.Namespace:
     :return: The namespace of arguments read from the command line.
     """
     parser = argparse.ArgumentParser()
+
     parser.add_argument('system_xml_path')
     parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-p', '--port', default=54321)
+    parser.add_argument('-a', '--address', default='[::]')
+    parser.add_argument('--no-serve', dest='do_serve', action='store_false', default=True)
 
     arguments = parser.parse_args()
     return arguments
@@ -24,9 +28,13 @@ def main():
     Entry point for the command line.
     """
     arguments = handle_user_arguments()
-    simulation_runner = Runner.from_xml_input(
-        input_xml=arguments.system_xml_path,
-    )
+
+    if arguments.do_serve:
+        simulation_runner = Server.from_xml_input(
+            input_xml=arguments.system_xml_path,
+            address=arguments.address,
+            port=arguments.port,
+        )
     simulation_runner.verbose = arguments.verbose
     simulation_runner.run()
 
