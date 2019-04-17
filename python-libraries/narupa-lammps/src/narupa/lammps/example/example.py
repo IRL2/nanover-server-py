9de595d9d53bd9575619aaed0255401dc5429800
Simon Bennie <simonbennie@gmail.com>
2019-04-17 15:05:31 +0100

    First working exteranl python program able to zero Lammps variables

--- python-libraries/narupa-lammps/src/narupa/lammps/example/example.py
+++ python-libraries/narupa-lammps/src/narupa/lammps/example/example.py
@@ -3,6 +3,13 @@ import numpy as np
 from MDAnalysis.tests.datafiles import PSF, DCD  # test trajectory
 from MDAnalysis import Universe
 from MDAnalysis.topology.guessers import guess_atom_element
+# from __future__ import print_function
+from lammps import lammps  # , PyLammps
+import mpi4py
+# from lammps import lammps #, PyLammps
+import sys
+from pprint import pprint
+from ctypes import *
 
 from narupa.protocol.trajectory import FrameData
 
@@ -14,29 +21,30 @@ element_index = {
     'S': 16
 }
 
-#U contains both topology and  positions
-u = MDAnalysis.Universe(PSF, DCD)  # always start with a Universe
-
+# U contains both topology and  positions
+# u = MDAnalysis.Universe(PSF, DCD)  # always start with a Universe
+#
 import time
-
+#
 from narupa.trajectory import FrameServer
-from narupa.mdanalysis import mdanalysis_to_frame_data
-
-
-frameServer = FrameServer(address='localhost', port=54321)
-###Send topolgy once at the begining of the server
-# Get topolgy in the right grpc format
-#takes u because it likes things in mdalaysis format
-topology_data = mdanalysis_to_frame_data(u, topology=True, positions=False)
-#print(topology_data)
-
-frame_index = 0
-#Now actually send the frame, only contains the topology at this stage
-frameServer.send_frame(frame_index, topology_data)
+# from narupa.mdanalysis import mdanalysis_to_frame_data
+#
+# frameServer = FrameServer(address='localhost', port=54321)
+# ###Send topolgy once at the begining of the server
+# # Get topolgy in the right grpc format
+# # takes u because it likes things in mdalaysis format
+# topology_data = mdanalysis_to_frame_data(u, topology=True, positions=False)
+# # print(topology_data)
+#
+# frame_index = 0
+# # Now actually send the frame, only contains the topology at this stage
+# frameServer.send_frame(frame_index, topology_data)
 
 print("Starting Trajectory Server")
-#frame_data = mdanalysis_to_frame_data(u, topology=False, positions=True)
-#print(frame_data)
+
+
+# frame_data = mdanalysis_to_frame_data(u, topology=False, positions=True)
+# print(frame_data)
 
 
 def lammps_to_frame_data(u: Universe, topology=True, positions=True) -> FrameData:
@@ -63,28 +71,71 @@ def lammps_to_frame_data(u: Universe, topology=True, positions=True) -> FrameDat
 
     return frame_data
 
+#L = lammps(ptr=lmp, comm=comm)
 
-
-while True:
-
-    for frame in u.trajectory:
-        #Frame data is in grpc format
-        frame_data = lammps_to_frame_data(u, topology=False, positions=True)
-        print("FRAME STUFF",frame_index,frame_data)
-
-        frameServer.send_frame(frame_index, frame_data)
-        time.sleep(1.0 / 30.0)
-        frame_index = frame_index + 1
-
-
-class LammpsThing:
+class LammpsHook:
     def __init__(self):
-        self.frame_server = FrameServer(address=..., port=...)
-        self.frame_index = 0
+        #Load MPI routines
+        from mpi4py import MPI
+        self.comm = MPI.COMM_WORLD
+        me = self.comm.Get_rank()
+        nprocs = self.comm.Get_size()
+        #Load frame server
+        #self.frame_server = FrameServer(address='localhost', port=54321)
+        #self.frame_index = 0
 
-    def hook(self, lmp):
-        l = lammps(ptr=lmp)
+    def testy(self):
+        #from mpi4py import MPI
+        #comm = MPI.COMM_WORLD
+        #me = comm.Get_rank()
+        #nprocs = comm.Get_size()
+        try:
+            L = lammps(ptr=lmp, comm=self.comm)
+        except LammmpsError:
+            print("Failed to load lammps wrapper")
+        L = lammps(comm=comm)# ptr=lmp, comm=comm)
+        print("inclass.testy")
+        n_atoms = L.get_natoms()
+        print("Atoms : ", n_atoms)
+
+    def LammpsHook(self, lmp):
+        # Make sure lammps object is callable
+        try:
+            L = lammps(ptr=lmp, comm=self.comm)
+        except Exception as e:
+            print("Failed to load lammps wrapper")
+            print(e)
+        n_atoms = L.get_natoms()
+        # mass = L.extract_atom("mass",2)
+        # xp = L.extract_atom("x",3)
+        # print("Natoms, mass, x[0][0] coord =",n_atoms,mass[1],xp[0][0])
+        # temp = L.extract_compute("thermo_temp",0,0)
+        # print("Temperature from compute =",temp)
+
+        n_local = L.extract_global('nlocal', 0)  # L.get_nlocal()
+        # print(tmp_force.__dict__)
+        # n3=3*n_atoms
+        v = L.gather_atoms("v", 1, 3)
+        for idx in range(n_atoms):
+            # print(type(v[idx]))
+            v[3 * idx + 0] *= 0.0000001
+            v[3 * idx + 1] *= 0.0000001
+            v[3 * idx + 2] *= 0.0000001
+        L.scatter_atoms("v", 1, 3, v)
+
         ...
-        frame_data = lammps_to_frame_data(...)
-        self.frame_server.send_frame(self.frame_index, frame_data)
-        self.frame_index += 1
+        # frame_data = lammps_to_frame_data(...)
+        # self.frame_server.send_frame(self.frame_index, frame_data)
+        # self.frame_index += 1
+
+
+# while True:
+#
+#     for frame in u.trajectory:
+#         # Frame data is in grpc format
+#         frame_data = lammps_to_frame_data(u, topology=False, positions=True)
+#         print("FRAME STUFF", frame_index, frame_data)
+#
+#         frameServer.send_frame(frame_index, frame_data)
+#         time.sleep(1.0 / 30.0)
+#         frame_index = frame_index + 1
