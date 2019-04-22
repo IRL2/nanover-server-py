ab90e2bf9a65e7ac679dde50a2a2ccaa4729859c
Jonathan Barnoud <jonathan@barnoud.net>
2019-04-22 12:02:03 +0100

    Separate arrays and values in FrameData

--- python-libraries/narupa-core/src/narupa/trajectory/frame_data.py
+++ python-libraries/narupa-core/src/narupa/trajectory/frame_data.py
@@ -7,20 +7,57 @@ class FrameData:
             self.raw = trajectory.FrameData()
         else:
             self.raw = raw_frame
+        self.values = ValuesView(self.raw)
+        self.arrays = ArraysView(self.raw)
+
+    def __contains__(self, key):
+        return key in self.arrays or key in self.values
+
+
+class RecordView:
+    record_name = None  # MUST be overwritten as "arrays" or "values"
+    singular = None  # MUST be overwritten as "array" or "value"
+
+    def __init__(self, raw):
+        if self.record_name is None:
+            raise NotImplemented(
+                'FieldView must be subclassed; record_name, singular, and'
+                '_converter must be overwritten.'
+            )
+        self._raw_field = getattr(raw, self.record_name)
 
     def __getitem__(self, key):
-        if key in self.raw.arrays:
-            field = self.raw.arrays[key]
-            return _convert_array_to_python(field)
-        if key in self.raw.values:
-            field = self.raw.values[key]
-            return _convert_value_to_python(field)
-        raise KeyError(f'No "{key}" key in the f{self.__class__.__name__}.')
+        if key in self:
+            field = self._raw_field[key]
+            return self._converter(field)
+        raise KeyError(f'No {self.singular} with the key "{key}".')
+
+    def __contains__(self, key):
+        return key in self._raw_field
+
+    def get(self, key, default=None):
+        if key in self:
+            return self[key]
+        return default
+
+    @staticmethod
+    def _converter(field):
+        raise NotImplemented('Subclasses must overwrite the _converter method.')
 
 
-def _convert_value_to_python(field):
-    return field.ListFields()[0][1]
+class ValuesView(RecordView):
+    record_name = 'values'
+    singular = 'value'
+
+    @staticmethod
+    def _converter(field):
+        return field.ListFields()[0][1]
 
 
-def _convert_array_to_python(field):
-    return field.ListFields()[0][1].values
+class ArraysView(RecordView):
+    record_name = 'arrays'
+    singular = 'array'
+
+    @staticmethod
+    def _converter(field):
+        return field.ListFields()[0][1].values
--- python-libraries/narupa-core/tests/trajectory/test_frame_data.py
+++ python-libraries/narupa-core/tests/trajectory/test_frame_data.py
@@ -1,4 +1,8 @@
+# TODO: tests for containers as values
+# TODO: tests for None as value
+
 import sys
+import numpy as np
 
 import pytest
 from hypothesis import strategies as st
@@ -9,6 +13,9 @@ from narupa.trajectory.frame_data import FrameData
 
 MAX_DOUBLE = sys.float_info.max
 MIN_DOUBLE = sys.float_info.min
+MAX_FLOAT32 = np.finfo(np.float32).max
+MIN_FLOAT32 = np.finfo(np.float32).min
+MAX_UINT32 = np.iinfo(np.uint32).max
 
 # This strategy generates a single value (i.e. not a container) that is valid
 # in as value in a FrameData, and that can be safely compared with "==" (i.e.
@@ -22,7 +29,13 @@ NUMBER_SINGLE_VALUE_STRATEGY = st.one_of(
     st.integers(min_value=MIN_DOUBLE, max_value=MAX_DOUBLE),
 )
 
-PYTHON_TYPES_TO_GRPC_ATTRIBUTE = {
+ARRAYS_STRATEGIES = {
+    'string_values': st.lists(st.text()),
+    'index_values': st.lists(st.integers(min_value=0, max_value=MAX_UINT32)),
+    'float_values': st.lists(st.floats(min_value=MIN_FLOAT32, max_value=MAX_FLOAT32)),
+}
+
+PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE = {
     int: 'number_value', float: 'number_value', str: 'string_value',
     bool: 'bool_value',
 }
@@ -44,19 +57,43 @@ def raw_frame_with_single_value(draw, value_strategy):
     """
     value = draw(value_strategy)
     raw = GrpcFrameData()
-    attribute_value = PYTHON_TYPES_TO_GRPC_ATTRIBUTE[type(value)]
+    attribute_value = PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE[type(value)]
     setattr(raw.values['sample.value'], attribute_value, value)
     return raw, value
 
 
-@given(raw_frame_with_single_value(st.floats()))
-def test_get_single_float_value(raw_frame_and_expectation):
+@st.composite
+def raw_frame_with_single_array(draw, value_strategy, field):
     """
-    We can get a float value by key from a FrameData.
+    Strategy to generate tuple of a GRPC FrameData with a single array
+    and the corresponding list.
+
+    The array is stored under the key "sample.array".
+
+    The return value is a tuple containing the GRP FrameData, and the
+    list corresponding to the stored array.
+
+    The array in the GRPC FrameData is taken from the strategy provided as
+    "value_strategy". The "field" argument tells under which field from the
+    ValueArray object the array must be stored.
+    """
+    value = draw(value_strategy)
+    raw = GrpcFrameData()
+    getattr(raw.arrays['sample.array'], field).values.extend(value)
+    return raw, value
+
+
+@given(raw_frame_with_single_value(st.floats()))
+def test_get_single_numeric_value(raw_frame_and_expectation):
+    """
+    We can get a numeric value by key from a FrameData.
+
+    Numeric values are stored as doubles by protobuf. We therefore cannot
+    compare their exact values.
     """
     raw_frame, expected_value = raw_frame_and_expectation
     frame = FrameData(raw_frame)
-    assert pytest.approx(frame['sample.value'], expected_value)
+    assert pytest.approx(frame.values['sample.value'], expected_value)
 
 
 @given(raw_frame_with_single_value(EXACT_SINGLE_VALUE_STRATEGY))
@@ -65,17 +102,56 @@ def test_get_single_exact_value(raw_frame_and_expectation):
     We can get a value by key from a FrameData.
 
     This test covers all non-container types that can be compared safely with
-    an equality check (i.e. not floats).
+    an equality check (i.e. not numeric).
     """
     raw_frame, expected_value = raw_frame_and_expectation
     frame = FrameData(raw_frame)
-    assert frame['sample.value'] == expected_value
+    assert frame.values['sample.value'] == expected_value
 
 
-def test_missing_key():
+# Arguments for the @given decorator cannot be parametrized. So we wrote a
+# separate test for each strategy. This function contains the logic of these
+# tests.
+def _test_get_exact_array(raw_frame_and_expectation):
+    raw_frame, expected_value = raw_frame_and_expectation
+    frame = FrameData(raw_frame)
+    assert frame.arrays['sample.array'] == expected_value
+
+
+@given(raw_frame_with_single_array(ARRAYS_STRATEGIES['string_values'], 'string_values'))
+def test_get_string_array(raw_frame_and_expectation):
+    """
+    We can get an array of strings from a FrameData.
+    """
+    _test_get_exact_array(raw_frame_and_expectation)
+
+
+@given(raw_frame_with_single_array(ARRAYS_STRATEGIES['index_values'], 'index_values'))
+def test_get_index_array(raw_frame_and_expectation):
+    """
+    We can get an array of indices from a FrameData.
+    """
+    _test_get_exact_array(raw_frame_and_expectation)
+
+
+@given(raw_frame_with_single_array(ARRAYS_STRATEGIES['float_values'], 'float_values'))
+def test_get_float_array(raw_frame_and_expectation):
+    """
+    We can get an array of floats from a FrameData.
+    """
+    raw_frame, expected_value = raw_frame_and_expectation
+    frame = FrameData(raw_frame)
+    assert all(
+        pytest.approx(actual, expected)
+        for actual, expected in zip(frame.arrays['sample.array'], expected_value)
+    )
+
+
+@pytest.mark.parametrize('record_name', ('values', 'arrays'))
+def test_missing_value_key(record_name):
     """
     We get a KeyError when requesting a key that does not exist.
     """
     frame = FrameData()
     with pytest.raises(KeyError):
-        frame['missing.key']
+        getattr(frame, record_name)['missing.key']
