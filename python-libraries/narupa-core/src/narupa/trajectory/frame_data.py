89294b5def738f7da434a3650d50cb36971e1adb
Jonathan Barnoud <jonathan@barnoud.net>
2019-04-25 16:46:29 +0100

    Add setters for python FrameData wrapper

--- python-libraries/narupa-core/src/narupa/trajectory/frame_data.py
+++ python-libraries/narupa-core/src/narupa/trajectory/frame_data.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import numbers
 from narupa.protocol import trajectory
 
 POSITIONS = 'particle.position'
@@ -6,6 +7,10 @@ ELEMENTS = 'particle.element'
 TYPES = 'particle.type'
 BONDS = 'bond'
 
+PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE = {
+    int: 'number_value', float: 'number_value', str: 'string_value',
+    bool: 'bool_value',
+}
 
 _Shortcut = namedtuple('_Shortcut', ['name', 'record_type', 'key', 'converter'])
 
@@ -82,27 +87,33 @@ class RecordView:
         if self.record_name is None or self.singular is None:
             raise NotImplementedError(
                 'FieldView must be subclassed; record_name, singular, and'
-                '_converter must be overwritten.'
+                '_convert_to_python must be overwritten.'
             )
-        self._raw_field = getattr(raw, self.record_name)
+        self._raw_record = getattr(raw, self.record_name)
 
     def __getitem__(self, key):
         if key in self:
-            field = self._raw_field[key]
-            return self._converter(field)
+            field = self._raw_record[key]
+            return self._convert_to_python(field)
         raise KeyError(f'No {self.singular} with the key "{key}".')
 
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
     def __contains__(self, key):
-        return key in self._raw_field
+        return key in self._raw_record
 
     def get(self, key, default=None):
         if key in self:
             return self[key]
         return default
 
+    def set(self, key, value):
+        raise NotImplementedError('Subclasses must overwrite the set method.')
+
     @staticmethod
-    def _converter(field):
-        raise NotImplementedError('Subclasses must overwrite the _converter method.')
+    def _convert_to_python(field):
+        raise NotImplementedError('Subclasses must overwrite the _convert_to_python method.')
 
 
 class ValuesView(RecordView):
@@ -110,8 +121,12 @@ class ValuesView(RecordView):
     singular = 'value'
 
     @staticmethod
-    def _converter(field):
+    def _convert_to_python(field):
         return field.ListFields()[0][1]
+
+    def set(self, key, value):
+        type_attribute = PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE[type(value)]
+        setattr(self._raw_record[key], type_attribute, value)
 
 
 class ArraysView(RecordView):
@@ -119,8 +134,28 @@ class ArraysView(RecordView):
     singular = 'array'
 
     @staticmethod
-    def _converter(field):
+    def _convert_to_python(field):
         return field.ListFields()[0][1].values
 
+    def set(self, key, value):
+        try:
+            reference_value = value[0]
+        except IndexError:
+            raise ValueError('Cannot decide what type to use for an empty object.')
+        except TypeError:
+            raise ValueError('Value must be indexable.')
 
+        if isinstance(reference_value, numbers.Integral) and reference_value >= 0:
+            type_attribute = 'index_values'
+        elif isinstance(reference_value, numbers.Real):
+            type_attribute = 'float_values'
+        elif isinstance(reference_value, str):
+            type_attribute = 'string_values'
+        else:
+            raise ValueError('Cannot decide what type to use.')
 
+        if key in self:
+            while self[key]:
+                self[key].pop()
+
+        getattr(self._raw_record[key], type_attribute).values.extend(value)
--- python-libraries/narupa-core/tests/trajectory/test_frame_data.py
+++ python-libraries/narupa-core/tests/trajectory/test_frame_data.py
@@ -9,7 +9,9 @@ from hypothesis import strategies as st
 from hypothesis import given
 
 from narupa.protocol.trajectory import FrameData as GrpcFrameData
-from narupa.trajectory.frame_data import FrameData, RecordView
+from narupa.trajectory.frame_data import (
+    FrameData, RecordView, PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE,
+)
 from narupa.trajectory import frame_data
 
 MAX_DOUBLE = sys.float_info.max
@@ -26,20 +28,16 @@ EXACT_SINGLE_VALUE_STRATEGY = st.one_of(
 )
 
 NUMBER_SINGLE_VALUE_STRATEGY = st.one_of(
-    st.floats(min_value=MIN_DOUBLE, max_value=MAX_DOUBLE),
+    st.floats(min_value=float(MIN_DOUBLE), max_value=float(MAX_DOUBLE)),
     st.integers(min_value=MIN_DOUBLE, max_value=MAX_DOUBLE),
 )
 
 ARRAYS_STRATEGIES = {
-    'string_values': st.lists(st.text()),
-    'index_values': st.lists(st.integers(min_value=0, max_value=MAX_UINT32)),
-    'float_values': st.lists(st.floats(min_value=MIN_FLOAT32, max_value=MAX_FLOAT32)),
+    'string_values': st.lists(st.text(), min_size=1),
+    'index_values': st.lists(st.integers(min_value=0, max_value=MAX_UINT32), min_size=1),
+    'float_values': st.lists(st.floats(min_value=float(MIN_FLOAT32), max_value=float(MAX_FLOAT32)), min_size=1),
 }
 
-PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE = {
-    int: 'number_value', float: 'number_value', str: 'string_value',
-    bool: 'bool_value',
-}
 
 
 @st.composite
@@ -222,7 +220,6 @@ def test_frame_data_contains(simple_frame, key, expected):
     assert (key in simple_frame) == expected
 
 
-
 @pytest.mark.parametrize('key, record_type, expected', (
     ('sample.string', 'values', 'foo bar'),
     ('array.string', 'arrays', ['foo', 'bar', 'toto']),
@@ -268,7 +265,7 @@ def test_partial_view_fails_record_name():
 
 def test_partial_view_fails_converter():
     """
-    If a subclass of RecordView is missing the '_converter' method, it fails.
+    If a subclass of RecordView is missing the '_convert_to_python' method, it fails.
     """
     class DummyView(RecordView):
         singular = 'dummy'
@@ -280,6 +277,22 @@ def test_partial_view_fails_converter():
     dummy = DummyView(raw)
     with pytest.raises(NotImplementedError):
         dummy.get('sample')
+
+
+def test_partial_view_fails_set():
+    """
+    If a subclass of RecordView is missing the 'set' method, it fails.
+    """
+    class DummyView(RecordView):
+        singular = 'dummy'
+        record_name = 'values'
+
+    raw = GrpcFrameData()
+    raw.values['sample'].string_value = 'foobar'
+
+    dummy = DummyView(raw)
+    with pytest.raises(NotImplementedError):
+        dummy.set('sample', 0)
 
 
 def test_positions_shortcut(simple_frame):
@@ -324,3 +337,99 @@ def test_missing_shortcut(exception):
     frame = FrameData()
     with pytest.raises(exception):
         frame.positions
+
+
+@given(EXACT_SINGLE_VALUE_STRATEGY)
+def test_set_new_exact_value(value):
+    frame = FrameData()
+    frame.values['sample.new'] = value
+    type_attribute = PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE[type(value)]
+    assert 'sample.new' in frame.raw.values
+    assert getattr(frame.raw.values['sample.new'], type_attribute) == value
+
+
+@given(NUMBER_SINGLE_VALUE_STRATEGY)
+def test_set_new_number_value(value):
+    frame = FrameData()
+    frame.values['sample.new'] = value
+    type_attribute = PYTHON_TYPES_TO_GRPC_VALUE_ATTRIBUTE[type(value)]
+    assert 'sample.new' in frame.raw.values
+    assert pytest.approx(getattr(frame.raw.values['sample.new'], type_attribute), value)
+
+
+@given(
+    initial_value=NUMBER_SINGLE_VALUE_STRATEGY,
+    new_value=EXACT_SINGLE_VALUE_STRATEGY,
+)
+def test_set_existing_value_number_to_exact(initial_value, new_value):
+    frame = FrameData()
+    frame.values['sample.new'] = initial_value
+    assert pytest.approx(frame.values['sample.new'], initial_value)
+    frame.values['sample.new'] = new_value
+    assert frame.values['sample.new'] == new_value
+
+
+@given(
+    initial_value=EXACT_SINGLE_VALUE_STRATEGY,
+    new_value=NUMBER_SINGLE_VALUE_STRATEGY,
+)
+def test_set_existing_value_exact_to_number(initial_value, new_value):
+    frame = FrameData()
+    frame.values['sample.new'] = initial_value
+    assert frame.values['sample.new'] == initial_value
+    frame.values['sample.new'] = new_value
+    assert pytest.approx(frame.values['sample.new'], new_value)
+
+
+@given(st.one_of(
+    ARRAYS_STRATEGIES['index_values'],
+    ARRAYS_STRATEGIES['string_values'],
+))
+def test_set_new_exact_array(value):
+    frame = FrameData()
+    frame.arrays['sample.new'] = value
+    assert frame.arrays['sample.new'] == value
+
+
+@given(ARRAYS_STRATEGIES['float_values'])
+def test_set_new_float_array(value):
+    frame = FrameData()
+    frame.arrays['sample.new'] = value
+    assert pytest.approx(frame.arrays['sample.new'], value)
+
+
+@given(ARRAYS_STRATEGIES['index_values'])
+def test_set_new_index_array(value):
+    frame = FrameData()
+    frame.arrays['sample.new'] = value
+    assert frame.arrays['sample.new'] == value
+
+
+@given(ARRAYS_STRATEGIES['string_values'])
+def test_set_new_string_array(value):
+    frame = FrameData()
+    frame.arrays['sample.new'] = value
+    assert frame.arrays['sample.new'] == value
+
+
+@given(
+    init_value=ARRAYS_STRATEGIES['index_values'],
+    new_value=ARRAYS_STRATEGIES['string_values'],
+)
+def test_set_existing_array(init_value, new_value):
+    frame = FrameData()
+    frame.arrays['sample.new'] = init_value
+    assert frame.arrays['sample.new'] == init_value
+    frame.arrays['sample.new'] = new_value
+    assert frame.arrays['sample.new'] == new_value
+
+
+@pytest.mark.parametrize('value', (
+    [],  # Empty list, type cannot be guessed
+    21,  # Not an sequence
+    [None, None],  # Not a valid type
+))
+def test_set_wrong_type_array_fails(value):
+    frame = FrameData()
+    with pytest.raises(ValueError):
+        frame.arrays['sample.net'] = value
