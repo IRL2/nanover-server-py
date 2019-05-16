5d8b8d9c0aa65e696db935d6d3bea03eba4264ad
Simon Bennie <simonbennie@gmail.com>
2019-05-16 15:08:19 +0100

    working element list being sent

--- python-libraries/narupa-lammps/src/narupa/lammps/hook.py
+++ python-libraries/narupa-lammps/src/narupa/lammps/hook.py
@@ -11,15 +11,15 @@ from lammps import lammps  # , PyLammps
 
 from narupa.protocol.trajectory import FrameData
 from narupa.trajectory import FrameServer, FrameData
-from narupa.trajectory.frame_data import POSITIONS
+from narupa.trajectory.frame_data import POSITIONS, ELEMENTS
 
 # Keep for converting internal LAMMPS atoms data into strings during testing
-element_index = {
-    'H': 1,
-    'C': 6,
-    'N': 7,
-    'O': 8,
-    'S': 16
+element_index_mass = {
+    1 :'H',
+    12:'C',
+    14:'N',
+    16:'O',
+    32:'S'
 }
 
 
@@ -74,6 +74,7 @@ class LammpsHook:
         port_no = 54321
         self.frame_server = FrameServer(address='[::]', port=port_no)
         self.frame_index = 0
+        self.frame_loop = 0
 
         # Load MPI routines, has to be performed here.
         from mpi4py import MPI
@@ -107,12 +108,11 @@ class LammpsHook:
 
         :param matrix_type: String identifying data to transmit, e.g x, v or f
         :param L: LAMMPS class that contains all the needed routines
-        :return: 3N matrix with all the data requested
+ type :return: 3N matrix with all the data requested
         """
 
         # n_local = L.extract_global('nlocal', 0)  # L.get_nlocal()
         # Hard to tell if LAMMPS python interpreter is working so for now print every step
-        print("In LAMMPS array")
         n_atoms = L.get_natoms()
         data_array = L.gather_atoms(matrix_type, 1, 3)
 
@@ -124,44 +124,68 @@ class LammpsHook:
         L.scatter_atoms(matrix_type, 1, 3, data_array)
         return data_array
 
-
     def gather_lammps_particle_types(self, L):
         '''
         Collect the paticle list from LAMMPS, this may be atomistic or coarse grained
-        particles
+        particles by looking up the ID of the atoms and that ID's corresponding mass
+        from the atoms_elements dict.
 
         :param L: LAMMPS class that contains all the needed routines
-        :return: 3N matrix with all the data requested
+        :return: 1N matrix with all the data requested
         '''
-        n_atoms = L.get_natoms()
-        data_array = L.gather_atoms(matrix_type, 1, 3)
 
-        return particle_list
+        # Extract the number of atoms types in the system.
+        ntypes = L.extract_global("ntypes", 0)
+        # Extract the masses of the types, 1D float of home many
+        # mass types were defined in input. Indexed from 1 not zero in lammps
+        atom_type_mass = L.extract_atom("mass", 2)
+        # Gather the atom types, 1D int of n atoms length.
+        atom_kind = L.gather_atoms("type", 0, 1)
 
 
-    def lammps_to_frame_data(self, data_array, topology=True, positions=True) -> FrameData:
+        # Atom mass is indexed from 1 in lammps for some reason.
+        # Create a new list rounded to the nearest mass integer
+        atom_mass_type = list(atom_type_mass[1:ntypes+1])
+        atom_mass_type = [round(x) for x in atom_mass_type]
+
+        atom_elements =[atom_mass_type[particle-1] for particle in atom_kind]
+
+
+        final_elements =[element_index_mass[mass] for mass in atom_elements]
+        #print(atom_mass_type)
+        #print(atom_kind[0:10])
+        #print(final_elements[0:10])
+        return final_elements
+
+    def lammps_array_to_frame_data(self, data_array, frame_data) -> FrameData:
         """
         Convert the flat ctype.c_double data into the framedata format.
-
+`
         :param data_array: Data to convert
         :param topology: Check if data is topological
         :param positions: Check if data is positional
         :return: overwrite data in data_array matrix with new formatted framedata
         """
-        try:
-            frame_data = FrameData()
-        except Exception as e:
-            raise Exception("Failed to load framedata", e)
 
-        if positions:
-            # print("Ndarray",np.ndarray(v))
-            # Copy the ctype array to numpy for processing
-            positions = np.fromiter(data_array, dtype=np.float, count=len(data_array))
-            # Convert to nm
-            positions = np.multiply(0.1, positions)
-            frame_data.arrays[POSITIONS] = positions
+        # Copy the ctype array to numpy for processing
+        positions = np.fromiter(data_array, dtype=np.float, count=len(data_array))
+        # Convert to nm
+        positions = np.multiply(0.1, positions)
+        frame_data.arrays[POSITIONS] = positions
 
+#    def lammps_elements_to_frame_data(self, data_array,  frame_data) -> FrameData:
+#        """
+#        Convert the flat ctype.c_double data into the framedata format.
+#
+#        :param data_array: Data to convert
+#        :param topology: Check if data is topological
+#        :param positions: Check if data is positional
+#        :return: overwrite data in data_array matrix with new formatted framedata
+#        """
+#
+#
         return frame_data
+
 
     # lammps_hook passes data between python and the Lammps binary
     def lammps_hook(self, lmp=None):
@@ -193,9 +217,32 @@ class LammpsHook:
             data_array = manipulate_dummy_array(matrix_type, n_atoms_dummy)
         else:
             data_array = self.manipulate_lammps_array(matrix_type, L)
+            atom_type = self.gather_lammps_particle_types(L)
 
-        self.frame_data = self.lammps_to_frame_data(data_array, positions=True, topology=False)
+        # Convert collected data to frame data
+        try:
+            frame_data = FrameData()
+        except Exception as e:
+            raise Exception("Failed to load framedata", e)
 
+        # Convert positions
+        self.frame_data = self.lammps_array_to_frame_data(data_array, frame_data)
+
+        # Remove once new pretend lammps object is made
+        # Convert elements
+        if lmp is not None:
+            self.frame_data.arrays[ELEMENTS] = atom_type
+            #self.frame_data = self.lammps_elements_to_frame_data(atom_type, frame_data)
+
+        # Send frame data
         # print("FRAME STUFF \n", self.frame_index, "\n", self.frame_data)
         self.frame_server.send_frame(self.frame_index, self.frame_data)
         self.frame_index += 1
+
+        self.frame_loop += 1
+        # Print every 100 cycles if python interperater is still running
+        if self.frame_loop == 10 :
+            logging.info("LAMMPS python fix is running step %s", self.frame_index)
+            logging.info("FRAME STUFF %s %s", self.frame_index, self.frame_data.raw)
+            self.frame_loop = 0
+
