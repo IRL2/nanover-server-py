ad9186f99c7fd2e650c9a851637c214e3b2bda07
Unknown <simonbennie@gmail.com>
2019-05-17 09:22:29 +0100

    Undo odd commit to MD analysis
    Undo

--- python-libraries/narupa-mdanalysis/src/narupa/mdanalysis/example/example.py
+++ python-libraries/narupa-mdanalysis/src/narupa/mdanalysis/example/example.py
@@ -25,7 +25,6 @@ print("Starting Trajectory Server")
 while True:
 
     for frame in u.trajectory:
-        #Frame data is in grpc format
         frame_data = mdanalysis_to_frame_data(u, topology=False, positions=True)
 
         frameServer.send_frame(frame_index, frame_data)
@@ -33,14 +32,3 @@ while True:
         frame_index = frame_index + 1
 
 
-class LammpsThing:
-    def __init__(self):
-        self.frame_server = FrameServer(address=..., port=...)
-        self.frame_index = 0
-
-    def hook(self, lmp):
-        l = lammps(ptr=lmp)
-        ...
-        frame_data = lammps_to_frame_data(...)
-        self.frame_server.send_frame(self.frame_index, frame_data)
-        self.frame_index += 1
