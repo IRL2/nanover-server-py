b919cdfde2b600f92caa8791eefcf1ae1b794780
mark <ragzouken@gmail.com>
2019-06-01 22:51:51 +0100

    cleanup

--- python-libraries/prototypes/curses_client.py
+++ python-libraries/prototypes/curses_client.py
@@ -12,32 +12,61 @@ import curses
 import colorsys
 import time
 
-import transformations
+from narupa.trajectory import FrameClient
 
-import grpc
-from narupa.protocol.trajectory import (
-    TrajectoryServiceStub,
-    GetFrameRequest,
-    GetFrameResponse,
-)
-from narupa.trajectory.frame_data import POSITIONS
+from transformations import rotation_matrix, scale_matrix
 
-cells = [" ", ".", "o", "O", "@"]
+character_sets = {
+    "boxes": [" ", "░", "▒", "▓", "█"],
+    "blobs": [" ", ".", "o", "O", "@"],
+}
 
-MAX_COLORS = 16
-CHAR_LOOKUP = None
-COLOR_LOOKUP = None
+class UserQuitException(Exception):
+    pass
 
-def render_positions_to_window(window, positions: np.ndarray):
-    xi = 0
-    yi = 1
-    zi = 2
+class Camera:
+    def __init__(self):
+        self.angle = 0
+        self.pitch = 0
+        self.scale = 5
+        self.origin = None
 
+    def get_matrix(self):
+        return (scale_matrix(self.scale, origin=self.origin)
+              @ rotation_matrix(self.angle, [0, 0, 1], point=self.origin)
+              @ rotation_matrix(self.pitch, [1, 0, 0], point=self.origin))
+
+class Timer:
+    def __init__(self):
+        self.last_time = time.time()
+
+    def get_delta(self):
+        return time.time() - self.last_time
+
+    def reset(self):
+        delta_time = self.get_delta()
+        self.last_time = time.time()
+        return delta_time
+
+class Renderer:
+    def __init__(self, characters, colors):
+        self.set_characters(characters)
+        self.set_colors(colors)
+
+    def set_characters(self, characters):
+        self.characters = characters
+        self.character_lookup = np.array(self.characters + [self.characters[-1]])
+
+    def set_colors(self, colors):
+        self.colors = colors
+        self.color_lookup = np.array(self.colors + [self.colors[-1]])
+
+def render_positions_to_window(window, positions: np.ndarray, renderer):
     h, w = window.getmaxyx()
 
-    positions[:,xi] += (w / 2)
-    positions[:,yi] += (h / 2)
-    positions[:,zi] *= 1000
+    positions[:,0] += (w / 2)
+    positions[:,1] += (h / 2)
+    positions[:,2] *= 1000
     np.round(positions, out=positions)
     positions = positions.astype(int)
 
@@ -45,10 +74,10 @@ def render_positions_to_window(window, positions: np.ndarray):
     index_buffer = np.full((w, h), -1, dtype=np.float32)
     rendered_cells = set()
 
-    min_depth = max_depth = positions[0][zi]
+    min_depth = max_depth = positions[0][2]
 
     for index, position in enumerate(positions):
-        x, y = position[xi], position[yi]
+        x, y = position[0], position[1]
 
         if x < 0 or x >= w or y < 0 or y >= h:
             continue
@@ -58,7 +87,7 @@ def render_positions_to_window(window, positions: np.ndarray):
         coord = y * w + x
 
         prev_depth = depth_buffer[x, y] if coord in rendered_cells else min_depth
-        this_depth = position[zi]
+        this_depth = position[2]
 
         min_depth = min(min_depth, this_depth)
         max_depth = max(max_depth, this_depth)
@@ -68,25 +97,25 @@ def render_positions_to_window(window, positions: np.ndarray):
             depth_buffer[x, y] = this_depth
             rendered_cells.add(coord)
 
-    char_count = len(cells)     
-    color_count = MAX_COLORS - 1
+    char_count = len(renderer.characters)     
+    color_count = len(renderer.colors) - 1
 
     # transform depths into cell indexes
     depth_buffer -= min_depth
     depth_buffer /= (max_depth - min_depth)
-    depth_buffer *= len(cells)
+    depth_buffer *= char_count
     depth_buffer.round(out=depth_buffer)
     depth_buffer = depth_buffer.astype(int)
 
     # transform particle indexes into color indexes
-    index_buffer *= (MAX_COLORS / len(positions))
+    index_buffer *= (color_count / len(positions))
     index_buffer %= 1
     index_buffer *= color_count
     index_buffer.round(out=index_buffer)
     index_buffer = index_buffer.astype(int)
 
-    char_buffer = CHAR_LOOKUP[depth_buffer]
-    color_buffer = COLOR_LOOKUP[index_buffer]
+    char_buffer = renderer.character_lookup[depth_buffer]
+    color_buffer = renderer.color_lookup[index_buffer]
 
     for y in range(h):
         for x in range(w):
@@ -97,136 +126,144 @@ def render_positions_to_window(window, positions: np.ndarray):
 
             window.addstr(y, x, char_buffer[x, y], color_buffer[x, y])
 
-def show_controls(window):
-    window.addstr(0, 0, "arrow keys -- rotate camera")
-    window.addstr(1, 0, " < >  keys -- zoom")
+def generate_colors(custom_colors=False):
+    max_colors = 8
 
-def write_trajectory_from_server(stdscr, *, address: str, port: int, custom_colors=False, boxes=False):
+    if curses.can_change_color() and custom_colors:
+        max_colors = 16
+        curses.init_color(0, 0, 0, 0)
+        for i in range(1, max_colors):
+            r, g, b = colorsys.hsv_to_rgb(i / max_colors, .5, 1)
+            curses.init_color(i, int(r * 1000), int(g * 1000), int(b * 1000))
+
+    for i in range(1, max_colors):
+        curses.init_pair(i, i, curses.COLOR_BLACK)
+
+    colors = [curses.color_pair(i) for i in range(max_colors)]
+
+    return colors
+
+def run_curses_client(stdscr, *, address: str, port: int, custom_colors=False, no_boxes=False):
     """
     Connect to a Narupa frame server and render the received frames to a curses 
     window.
 
-    :param stdscr: curses window to render to.
+    :param stdscr: Curses window to render to.
     :param address: Host name to connect to.
     :param port: Port to connect to on the host.
+    :param custom_colors: Whether to modify the terminal colors.
+    :param no_boxes: Whether to avoid box shading characters.
     """
-    global MAX_COLORS
+
+    colors = generate_colors(custom_colors=custom_colors)
+    characters = character_sets["boxes"] if not no_boxes else character_sets["blobs"]
+    renderer = Renderer(characters, colors)
+
+    camera = Camera()
+    fps_timer = Timer()
+    render_timer = Timer()
 
     stdscr.clear()
     stdscr.nodelay(True)
     stdscr.addstr(0, 0, "Connecting...")
     stdscr.refresh()
 
-    if boxes:
-        cells[:] = [" ", "░", "▒", "▓", "█"]
-
-    for i in range(1, MAX_COLORS):
-        curses.init_pair(i, i, curses.COLOR_BLACK)
-
-    if curses.can_change_color() and custom_colors:
-        curses.init_color(0, 0, 0, 0)
-        for i in range(1, MAX_COLORS):
-            r, g, b = colorsys.hsv_to_rgb(i / MAX_COLORS, .5, 1)
-            curses.init_color(i, int(r * 1000), int(g * 1000), int(b * 1000))
-    else:
-        MAX_COLORS = 8
-
-    host = '{}:{}'.format(address, port)
-    channel = grpc.insecure_channel(host)
-    stub = TrajectoryServiceStub(channel)
-    frame_iter = stub.SubscribeFrames(GetFrameRequest())
+    client = FrameClient(address=address, port=port)
 
     stdscr.clear()
     stdscr.addstr(0, 0, "Connected.")
 
-    angle = 0
-    angle2 = 0
-    scale = 5
+    latest_frame = None
 
-    global COLOR_LOOKUP, CHAR_LOOKUP
-    thing = list(cells)
-    thing.append(thing[-1])
-    CHAR_LOOKUP = np.array(thing)
+    def get_frame(frame_index, frame):
+        nonlocal latest_frame
+        latest_frame = frame
 
-    thing = [curses.color_pair(i) for i in range(MAX_COLORS)]
-    thing.append(thing[-1])
-    COLOR_LOOKUP = np.array(thing)
+    client.subscribe_frames_async(get_frame)
 
-    for i, frame in enumerate(frame_iter):
-        start_time = time.time()
+    def show_controls(window):
+        window.addstr(0, 0, "arrow keys -- rotate camera")
+        window.addstr(1, 0, " < >  keys -- zoom")
 
-        positions = frame_to_ndarray(frame)
+    def rotate_plus():
+        camera.angle += .1
+    def rotate_minus():
+        camera.angle -= .1
+    def pitch_plus():
+        camera.pitch += .1
+    def pitch_minus():
+        camera.pitch -= .1
+    def zoom_in():
+        camera.scale *= .9
+    def zoom_out():
+        camera.scale /= .9
+    def quit():
+        raise UserQuitException()
 
-        center = np.sum(positions, axis=0) / len(positions)
-        camera = transformations.scale_matrix(scale, origin=center)
-        camera = camera @ transformations.rotation_matrix(angle, [0, 0, 1], point=center)
-        camera = camera @ transformations.rotation_matrix(angle2, [1, 0, 0], point=center)
+    bindings = {
+        curses.KEY_LEFT:  rotate_plus,
+        curses.KEY_RIGHT: rotate_minus,
+        curses.KEY_UP:    pitch_plus,
+        curses.KEY_DOWN:  pitch_minus,
+        ord(","):         zoom_in,
+        ord("."):         zoom_out,
+        ord("q"):         quit,
+    }
 
-        positions = np.append(positions, np.ones((len(positions), 1)), axis=-1)
-        pos = positions[0]
-        positions = positions @ camera
+    def check_input():
+        char = stdscr.getch()
 
-        #stdscr.addstr(0, 0, "Frame {}: ({} positions)".format(i, len(positions)))
-
-        stdscr.clear()
-
-        #show_controls(stdscr)
-
-        render_positions_to_window(stdscr, positions)
-        show_controls(stdscr)
-        #stdscr.refresh()
-
-        c = stdscr.getch()
-
-        if c == ord("q"):
-            break
-        if c == curses.KEY_LEFT:
-            angle += 0.1
-        if c == curses.KEY_RIGHT:
-            angle -= 0.1
-        if c == curses.KEY_UP:
-            angle2 += 0.1
-        if c == curses.KEY_DOWN:
-            angle2 -= 0.1
-        if c == ord(","):
-            scale *= .9
-        if c == ord("."):
-            scale /= .9
+        if char in bindings:
+            bindings[char]()
 
         curses.flushinp()
 
+    def process_frame(frame):
+        # convert positions from frame
+        positions = np.array(frame.particle_positions, dtype=np.float32).reshape((-1, 3))
+
+        # center the camera
+        camera.origin = np.sum(positions, axis=0) / len(positions)
+
+        # add w component to positions then multiply by camera matrix
+        positions = np.append(positions, np.ones((len(positions), 1)), axis=-1)
+        positions = positions @ camera.get_matrix()
+
+        return positions
+
+    def render(frame):
+        positions = process_frame(frame)
+
+        stdscr.clear()
+
+        render_positions_to_window(stdscr, positions, renderer)
+        
+        show_controls(stdscr)
+
         h, w = stdscr.getmaxyx()
-        stdscr.addstr(h - 1, 0, "{0:.3} fps".format(1.0 / (time.time() - start_time)))
+        stdscr.addstr(h - 1, 0, "{0:.3} fps".format(1.0 / fps_timer.reset()))
         stdscr.refresh()
 
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Closing...")
-    stdscr.refresh()
+    def loop():
+        check_input()
 
-    channel.close()
+        if render_timer.get_delta() < 0.05 or latest_frame is None:
+            return
 
+        render_timer.reset()
 
-def frame_to_ndarray(frame: GetFrameResponse) -> np.ndarray:
-    """
-    Convert a frame received from the Narupa server to an array of coordinates.
+        render(latest_frame)
 
-    .. note::
-
-        The frame is assumed to describes particles in a 3D space.
-
-    :param frame: A frame received from a Narupa server.
-    :return: A numpy array of ``np.float32`` with one row per atom, and 3
-        columns. The coordinates are expressed in ångstöm.
-    """
-    raw_positions = (
-        frame
-        .frame.arrays[POSITIONS]
-        .float_values
-        .values
-    )
-    # Here we assume that the coordinates are in 3 dimensions.
-    positions = np.array(raw_positions, dtype=np.float32).reshape((-1, 3))
-    return positions
+    try:
+        while True:
+            loop()
+    except UserQuitException:
+        pass
+    finally:
+        stdscr.clear()
+        stdscr.addstr(0, 0, "Closing...")
+        stdscr.refresh()
+        client.close()
 
 
 def handle_user_args() -> argparse.Namespace:
@@ -239,20 +276,20 @@ def handle_user_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--host', default='localhost')
     parser.add_argument('--port', type=int, default=8000)
-    parser.add_argument('--rainbow', action="store_true", default=False)
-    parser.add_argument('--boxes', action="store_true")
+    parser.add_argument('--rainbow', action="store_true")
+    parser.add_argument('--no-boxes', action="store_true")
     arguments = parser.parse_args()
     return arguments
 
 
 def main(stdscr):
     arguments = handle_user_args()
-    write_trajectory_from_server(
+    run_curses_client(
         stdscr,
         address=arguments.host,
         port=arguments.port,
         custom_colors=arguments.rainbow,
-        boxes=arguments.boxes,
+        no_boxes=arguments.no_boxes,
     )
 
 
