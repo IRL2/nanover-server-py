Fix units for box vectors in ASE

python-libraries/narupa-ase/src/narupa/ase/converter.py
@@ -134,9 +134,12 @@ def add_ase_box_vectors_to_frame_data(data: FrameData, ase_atoms: Atoms):
     """
     Adds the periodic box vectors to the frame.
 
-    The box vectors form the 3x3 matrix that describes a triclinic box.
+    The box vectors form the 3x3 matrix that describes a triclinic box. The
+    box is only added if the system is periodic.
     """
-    data.box_vectors = ase_atoms.cell * ANG_TO_NM
+    box_vectors = ase_atoms.cell.copy()
+    box_vectors[np.diag_indices_from(box_vectors)] *= ANG_TO_NM
+    data.box_vectors = box_vectors
 
 
 def add_ase_topology_to_frame_data(frame_data: FrameData, ase_atoms: Atoms):