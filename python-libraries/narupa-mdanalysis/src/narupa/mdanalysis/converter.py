be551026f15ff544678bea8c0b08856e8fd7d504
Mike O'Connor <m.oconnor@bristol.ac.uk>
2019-09-27 16:58:10 +0100

    add counts to mdanalysis converter.

--- python-libraries/narupa-mdanalysis/src/narupa/mdanalysis/converter.py
+++ python-libraries/narupa-mdanalysis/src/narupa/mdanalysis/converter.py
@@ -14,12 +14,21 @@ element_index = {
 
 
 def mdanalysis_to_frame_data(u: Universe, topology=True, positions=True) -> FrameData:
+    """
+    Converts from an MDAnalysis universe to Narupa FrameData object.
+    :param u: MDAnalysis universe.
+    :param topology: Whether to include topology.
+    :param positions: Whether to include positions.
+    :return: Frame data constructed from MDAnalysis universe.
+    """
     frame_data = FrameData()
 
     if topology:
         frame_data.residue_names = u.residues.resnames
         frame_data.residue_chains = u.residues.segindices
         frame_data.particle_names = u.atoms.names
+        frame_data.residue_count = len(u.residues)
+        frame_data.chain_count = len(u.segments)
         elements = [element_index[guess_atom_element(name)] for name in u.atoms.names]
         frame_data.particle_elements = elements
         frame_data.particle_residues = u.atoms.resids
@@ -27,5 +36,7 @@ def mdanalysis_to_frame_data(u: Universe, topology=True, positions=True) -> Fram
 
     if positions:
         frame_data.particle_positions = u.atoms.positions * 0.1
+
+    frame_data.particle_count = len(u.atoms)
 
     return frame_data
