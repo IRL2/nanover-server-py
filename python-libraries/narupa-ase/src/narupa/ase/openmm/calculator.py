fd1a010de46ea756d106a4f63a26b53c53e3db03
Mike O'Connor <mikeoconnor0308@gmail.com>
2019-10-14 16:43:46 +0000

    Apply suggestion to python-libraries/narupa-ase/src/narupa/ase/openmm/calculator.py

--- python-libraries/narupa-ase/src/narupa/ase/openmm/calculator.py
+++ python-libraries/narupa-ase/src/narupa/ase/openmm/calculator.py
@@ -23,7 +23,7 @@ class OpenMMCalculator(Calculator):
     the given OpenMM simulation.
 
     :param simulation: An OpenMM simulation.
-    :param atoms ASE :class: Atoms to use with the calculator. The topology of the ASE atoms should be consistent
+    :param atoms: ASE :class:`Atoms` to use with the calculator. The topology of the ASE atoms should be consistent
     with the OpenMM simulation. See :method: OpenMMCalculator.generate_atoms for a helper function to generate a
     compatible ASE atoms object.
 
