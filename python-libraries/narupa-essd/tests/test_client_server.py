29d5ab51309720084728963acc36f6aacd666a1a
Mike O'Connor <m.oconnor@bristol.ac.uk>
2019-10-22 17:47:19 +0100

    remove localhost test, too specific for CI

--- python-libraries/narupa-essd/tests/test_client_server.py
+++ python-libraries/narupa-essd/tests/test_client_server.py
@@ -22,16 +22,6 @@ def test_send_service(client, server, service):
     assert service in services
 
 
-def test_send_service_localhost(client, server, service):
-    properties = dict(service.properties)
-    properties['address'] = 'localhost'
-    service = ServiceHub(**properties)
-    server.register_service(service)
-    services = client.search_for_services(search_time=0.4, interval=0.001)
-    assert len(services) == 1
-    assert ServiceHub(name=service.name, address='127.0.0.1', id=service.id) in services
-
-
 def test_send_service_all_interfaces(client, server, service):
     properties = dict(service.properties)
     properties['address'] = '[::]'
