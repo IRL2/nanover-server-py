8d69e1386dfe796b55d4f905b0a6ae489a9013c2
Mike O'Connor <m.oconnor@bristol.ac.uk>
2019-11-18 15:15:23 +0000

    add multiplayer testing for narupa imd client
    the narupa imd client class was missing multiplayer support in the tests.

--- python-libraries/narupa-core/tests/test_client.py
+++ python-libraries/narupa-core/tests/test_client.py
@@ -2,6 +2,8 @@ import time
 
 import grpc
 import pytest
+
+from narupa.multiplayer import MultiplayerServer
 from .test_frame_server import simple_frame_data, frame_server
 from .imd.test_imd_server import imd_server, interaction
 from narupa.app.client import NarupaClient
@@ -9,15 +11,22 @@ import numpy as np
 
 
 @pytest.fixture
-def client_server(frame_server, imd_server):
+def multiplayer_server():
+    server = MultiplayerServer(address='localhost', port=0)
+    yield server
+    server.close()
+
+@pytest.fixture
+def client_server(frame_server, imd_server, multiplayer_server):
     client = NarupaClient(trajectory_port=frame_server.port,
-                          imd_port=imd_server.port)
-    yield client, frame_server, imd_server
+                             imd_port=imd_server.port,
+                             multiplayer_port=multiplayer_server.port)
+    yield client, frame_server, imd_server, multiplayer_server
     client.close()
 
 
 def test_receive_frames(client_server, simple_frame_data):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     time.sleep(0.2)
     frame_server.send_frame(0, simple_frame_data)
     time.sleep(0.5)
@@ -28,7 +37,7 @@ def test_receive_frames(client_server, simple_frame_data):
 
 
 def test_receive_multiple_frames(client_server, simple_frame_data):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     # wait for client to connect.
     time.sleep(0.1)
     frame_server.send_frame(0, simple_frame_data)
@@ -38,7 +47,7 @@ def test_receive_multiple_frames(client_server, simple_frame_data):
 
 
 def test_reconnect_receive(client_server, simple_frame_data):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     frame_server.send_frame(0, simple_frame_data)
     client.close()
     assert client.latest_frame is None
@@ -48,7 +57,7 @@ def test_reconnect_receive(client_server, simple_frame_data):
 
 
 def test_close_interaction(client_server, interaction):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     id = client.start_interaction(interaction)
     time.sleep(0.2)
     assert len(imd_server.service.active_interactions) == 1
@@ -61,12 +70,12 @@ def test_running_imd(client_server):
     """
     Test that a client attempts to connect to IMD by default.
     """
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     assert client.running_imd
 
 
 def test_stop_interaction(client_server, interaction):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     id = client.start_interaction(interaction)
     time.sleep(0.2)
     assert len(imd_server.service.active_interactions) == 1
@@ -76,14 +85,14 @@ def test_stop_interaction(client_server, interaction):
 
 
 def test_start_interaction(client_server, interaction):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     client.start_interaction(interaction)
     time.sleep(0.2)
     assert len(imd_server.service.active_interactions) == 1
 
 
 def test_update_interaction(client_server, interaction):
-    client, frame_server, imd_server = client_server
+    client, frame_server, imd_server, multiplayer_server = client_server
     id = client.start_interaction(interaction)
     interaction.position = [2, 2, 2]
     client.update_interaction(id, interaction)
@@ -92,22 +101,22 @@ def test_update_interaction(client_server, interaction):
     assert np.allclose(list(imd_server.service.active_interactions.values())[0].position, (2, 2, 2))
 
 
-def test_no_imd(frame_server, interaction):
-    client = NarupaClient(run_imd=False, trajectory_port=frame_server.port)
+def test_no_imd(frame_server, multiplayer_server, interaction):
+    client = NarupaClient(run_imd=False, trajectory_port=frame_server.port, multiplayer_port=multiplayer_server.port)
     with pytest.raises(ValueError):
         client.start_interaction(interaction)
     client.close()
 
 
-def test_no_imd_update(frame_server, interaction):
-    client = NarupaClient(run_imd=False, trajectory_port=frame_server.port)
+def test_no_imd_update(frame_server, multiplayer_server, interaction):
+    client = NarupaClient(run_imd=False, trajectory_port=frame_server.port, multiplayer_port=multiplayer_server.port)
     with pytest.raises(ValueError):
         client.update_interaction(0, interaction)
     client.close()
 
 
-def test_no_imd_stop(frame_server, interaction):
-    client = NarupaClient(run_imd=False, trajectory_port=frame_server.port)
+def test_no_imd_stop(frame_server, multiplayer_server, interaction):
+    client = NarupaClient(run_imd=False, trajectory_port=frame_server.port, multiplayer_port=multiplayer_server.port)
     with pytest.raises(ValueError):
         client.stop_interaction(0)
     client.close()
