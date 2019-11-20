94090857a13860f33eaf0ef5e92bcd30d7027b9c
Mike O'Connor <m.oconnor@bristol.ac.uk>
2019-11-20 17:39:11 +0000

    add tests for trajectory writing

--- python-libraries/narupa-ase/src/narupa/ase/trajectory_logger.py
+++ python-libraries/narupa-ase/src/narupa/ase/trajectory_logger.py
@@ -11,15 +11,11 @@ class TrajectoryLogger:
     :param atoms: ASE :class:`Atoms` from which to write data.
     :param filename: Path to filename to write to.
     :param format: Format to use, as supported by ASE. If not specified, derived from filename.
-    :param parallel: 
-    :param kwargs:
+    :param parallel:  Default is to write on master process only.  Set to `False` to write from all processes.
+    :param kwargs: Keyword arguments to be passed to the underlying :fun:`ase.io.write` method.
     """
 
     def __init__(self, atoms: Atoms, filename: str, format: Optional[str] = None, parallel=True, **kwargs):
-        """
-
-
-        """
         self.frame_index = 0
         self.atoms = atoms
         self.filename = filename
@@ -28,6 +24,10 @@ class TrajectoryLogger:
         self._kwargs = kwargs
 
     def write(self):
+        """
+        Writes the current state of the atoms, overwriting if this is the first time the method has been
+        called, appending otherwise.
+        """
         should_append = self.frame_index != 0
         ase.io.write(self.filename,
                      self.atoms,
@@ -35,6 +35,7 @@ class TrajectoryLogger:
                      parallel=self.parallel,
                      append=should_append,
                      **self._kwargs)
+        self.frame_index += 1
 
     def __call__(self):
         self.write()
---
+++ python-libraries/narupa-ase/tests/test_trajectory_logger.py
@@ -0,0 +1,65 @@
+import os
+import shutil
+import tempfile
+import time
+from collections import Collection
+from typing import NamedTuple, List
+
+import pytest
+from ase import Atoms
+
+from narupa.ase.trajectory_logger import TrajectoryLogger
+from test_imd_reset import fcc_atoms
+from ase.io import read, write
+import numpy as np
+
+SUPPORTED_EXTENSIONS = ['xyz', 'gromacs', 'netcdftrajectory']
+FRAMES = 10
+
+@pytest.fixture()
+def atoms():
+    return fcc_atoms()
+
+
+@pytest.fixture(scope="session")
+def tmp_dir(tmpdir_factory):
+    path = tmpdir_factory.mktemp('outputs')
+    return path
+
+
+def check_file_images(images: List[Atoms], file, symbols=True, positions=True, cell=True):
+    with open(file, 'r') as f:
+        debug = f.read()
+
+    new_images = read(file, index=':')
+    assert len(new_images) == len(images)
+    for atoms, new_atoms in zip(images, new_images):
+        assert len(atoms) == len(new_atoms)
+        if positions:
+            assert np.allclose(atoms.positions, new_atoms.positions, atol=0.01)
+        if symbols:
+            assert atoms.get_chemical_symbols() == new_atoms.get_chemical_symbols()
+        if cell:
+            assert np.allclose(atoms.get_cell(), new_atoms.get_cell())
+
+
+@pytest.mark.parametrize('ext', SUPPORTED_EXTENSIONS)
+def test_write(atoms, tmp_dir, ext):
+    file = os.path.join(tmp_dir, "atoms" + "." + ext)
+
+    logger = TrajectoryLogger(atoms, file)
+    logger.write()
+
+    assert os.path.exists(file)
+    check_file_images([atoms], file)
+
+@pytest.mark.parametrize('ext', SUPPORTED_EXTENSIONS)
+def test_write_multiple_frames(atoms, tmp_dir, ext):
+    file = os.path.join(tmp_dir, "atoms" + "." + ext)
+
+    logger = TrajectoryLogger(atoms, file)
+    for i in range(FRAMES):
+        logger.write()
+
+    assert os.path.exists(file)
+    check_file_images([atoms] * FRAMES, file)
