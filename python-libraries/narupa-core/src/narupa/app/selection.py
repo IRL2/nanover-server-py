b44e542091a25450adbac4c519849f661a7a0636
Alex Binnie <alexjbinnie@gmail.com>
2019-12-03 16:41:18 +0000

    Apply suggestion to python-libraries/narupa-core/src/narupa/app/selection.py

--- python-libraries/narupa-core/src/narupa/app/selection.py
+++ python-libraries/narupa-core/src/narupa/app/selection.py
@@ -264,6 +264,7 @@ class NarupaImdSelection:
 def get_nested_or_default(dict: Dict, default: object, *keys: Iterable[str]) -> object:
     """
     Iterate down a nested dictionary by accessing subsequent keys, returning the default if at any point a key is not found.
+
     :param dict: The dictionary to iterate.
     :param default: The default value if a key is not found.
     :param keys: The keys to look up recursively in the dictionary.
