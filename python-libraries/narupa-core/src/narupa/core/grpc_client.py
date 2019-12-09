f986acc44d4ea88ec4446c040e29132b2748caf6
mark <ragzouken@gmail.com>
2019-12-09 11:29:21 +0000

    remove unused types

--- python-libraries/narupa-core/src/narupa/core/grpc_client.py
+++ python-libraries/narupa-core/src/narupa/core/grpc_client.py
@@ -1,7 +1,7 @@
 # Copyright (c) Intangible Realities Lab, University Of Bristol. All rights reserved.
 # Licensed under the GPL. See License.txt in the project root for license information.
 from concurrent import futures
-from typing import Optional, Any, Callable
+from typing import Optional
 
 import grpc
 from narupa.core import DEFAULT_CONNECT_ADDRESS
