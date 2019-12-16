0773c74c8c54e7d1b88aa31f3f844bfd66fda1b0
Unknown <mikeoconnor0308@gmail.com>
2019-12-16 16:26:01 +0000

    add some more docs

--- python-libraries/narupa-ase/src/narupa/ase/trajectory_logger.py
+++ python-libraries/narupa-ase/src/narupa/ase/trajectory_logger.py
@@ -18,6 +18,15 @@ class TrajectoryLogger:
     """
     Trajectory logging class for use with ASE simulations.
 
+    Can be attached to an ASE simulation, resulting in frames being written automatically:
+
+    >>> from ase.calculators.emt import EMT
+    >>> from ase.lattice.cubic import FaceCenteredCubic
+    >>> atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]], symbol="Cu", size=(2, 2, 2), pbc=True)
+    >>> atoms.set_calculator(EMT())
+    >>> dynamics = Langevin(atoms, timestep=0.5, temperature=300 * units.kB, friction=1.0)
+    >>> dynamics.attach(TrajectoryLogger(atoms, 'example.xyz'), interval=10) # attach an XYZ logger.
+
     :param atoms: ASE :class:`Atoms` from which to write data.
     :param filename: Path to filename to write to.
     :param format: Format to use, as supported by ASE. If not specified, derived from filename.
