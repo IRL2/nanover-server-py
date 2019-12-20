82bcc9804a642eb820c9c8105b336b729e3675eb
Unknown <simonbennie@gmail.com>
2019-12-20 09:59:18 +0000

    change a filename to lowercase and make the conversion tool commented

--- python-libraries/narupa-lammps/maintainer/md_temp.py
+++ python-libraries/narupa-lammps/maintainer/md_temp.py
@@ -1,37 +1,33 @@
+# This is a valueing routine for various conversion factiors in mdanalysis,
+# this is just used as a useful sanity checking tool for varous conversions.
+
 import MDAnalysis
-#import quantities as pq
-#q = 1 * pq.newton
-#q.units = pq.m
-#q9 = q.simplified
-#pq.
 #print(q)
-hold = MDAnalysis.units.convert(1,"Newton","kJ/(mol*nm)")
-print(hold)
+value = MDAnalysis.units.convert(1,"Newton","kJ/(mol*nm)")
+print(value)
 
 
-hold = MDAnalysis.units.convert(1,"Newton","kJ/(mol*nm)")
-print("Newton", hold)
+value = MDAnalysis.units.convert(1,"Newton","kJ/(mol*nm)")
+print("Newton", value)
 
-hold = MDAnalysis.units.convert(0.0000001,"Newton","kJ/(mol*nm)")
-print("picogram-micrometer/microsecond", hold)
+value = MDAnalysis.units.convert(0.0000001,"Newton","kJ/(mol*nm)")
+print("picogram-micrometer/microsecond", value)
 
-hold = MDAnalysis.units.convert(0.001,"Newton","kJ/(mol*nm)")
-print("atoogram-nanometer/nanosecond", hold)
+value = MDAnalysis.units.convert(0.001,"Newton","kJ/(mol*nm)")
+print("atoogram-nanometer/nanosecond", value)
 
-hold = MDAnalysis.units.convert(1/100000,"Newton","kJ/(mol*nm)")
-print("Dyne", hold)
+value = MDAnalysis.units.convert(1/100000,"Newton","kJ/(mol*nm)")
+print("Dyne", value)
 
+value = MDAnalysis.units.convert(1,"kcal/(mol*Angstrom)","kJ/(mol*nm)")
+print("Kcal", value)
 
-hold = MDAnalysis.units.convert(1,"kcal/(mol*Angstrom)","kJ/(mol*nm)")
-print("Kcal", hold)
+value = MDAnalysis.units.convert(1,"eV","kJ/mol")
+print("eV", value)
+print("eV/nm", value/10)
 
+value = MDAnalysis.units.convert(1,"bohr","nm")
+print("bhor- > nm", value)
 
-hold = MDAnalysis.units.convert(1,"eV","kJ/mol")
-print("eV", hold)
-print("eV/nm", hold/10)
-
-#hold = MDAnalysis.units.convert(1,"bohr","nm")
-#print("bhor- > nm", hold)
-
-hold = MDAnalysis.units.convert(1,"eV","kJ/mol")
-print("eV", hold)
+value = MDAnalysis.units.convert(1,"eV","kJ/mol")
+print("eV", value)
