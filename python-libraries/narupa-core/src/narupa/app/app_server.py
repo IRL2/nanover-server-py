874ac2b849dc8fb39b56f36fd5ebe03f698b31d4
Mike O'Connor <m.oconnor@bristol.ac.uk>
2020-01-09 08:23:17 +0000

    continue sketching out app level server

--- python-libraries/narupa-core/src/narupa/app/app_server.py
+++ python-libraries/narupa-core/src/narupa/app/app_server.py
@@ -5,6 +5,7 @@ with an underyling gRPC server, discovery, multiplayer and commands.
 from narupa.core import NarupaServer
 from narupa.essd import DiscoveryServer
 from narupa.multiplayer.multiplayer_service import MultiplayerService
+from narupa.protocol.multiplayer import add_MultiplayerServicer_to_server
 
 
 class NarupaApplicationServer:
@@ -12,14 +13,20 @@ class NarupaApplicationServer:
     def __init__(self, address=None, port=0):
         self._server = NarupaServer(address=address, port=port)
         self._discovery = DiscoveryServer()
-        self._services = []
+        self._services = set()
+        self._update_discovery_services()
 
-    def setup_services(self):
+    def _setup_multiplayer(self):
         self._multiplayer = MultiplayerService()
-
-
+        self._server.add_service(self._multiplayer, add_MultiplayerServicer_to_server)
+        self._services.add(self._multiplayer)
+    
+    
     def close(self):
         self._discovery.close()
         for service in self._services:
             service.close()
+        self._server.close()
 
+    def _update_discovery_services(self):
+        pass
