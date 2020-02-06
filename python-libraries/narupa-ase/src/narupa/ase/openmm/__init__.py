9f6a4f154efcff57e9ba2873a7d4d11033c10089
Unknown <mikeoconnor0308@gmail.com>
2020-02-06 14:51:28 +0000

    implement openmm example with neuraminidase

--- python-libraries/narupa-ase/src/narupa/ase/openmm/__init__.py
+++ python-libraries/narupa-ase/src/narupa/ase/openmm/__init__.py
@@ -1,8 +1,7 @@
 # Copyright (c) Intangible Realities Lab, University Of Bristol. All rights reserved.
 # Licensed under the GPL. See License.txt in the project root for license information.
 from .calculator import OpenMMCalculator
-from .runner import OpenMMIMDRunner
-
+from .runner import OpenMMIMDRunner, openmm_ase_frame_adaptor
 """
 Module containing ASE calculators and runners for running Narupa compatible OpenMM simulations with ASE.
 """
