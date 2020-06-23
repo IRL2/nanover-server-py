ce151d06ed162042f0083199efb828de9aad6fec
Mark Wonnacott <ragzouken@gmail.com>
2020-06-23 10:18:18 +0100

    removed added whitespace
    Former-commit-id: 85060164ff5137a9d2e95c8c2635fd725e869664

--- python-libraries/narupa-core/src/narupa/imd/particle_interaction.py
+++ python-libraries/narupa-core/src/narupa/imd/particle_interaction.py
@@ -7,7 +7,6 @@ import math
 from typing import Dict, Any, Iterable
 
 import numpy as np
-
 import narupa.protocol.imd.imd_pb2 as imd_pb2
 from narupa.utilities.protobuf_utilities import dict_to_struct, struct_to_dict
 
