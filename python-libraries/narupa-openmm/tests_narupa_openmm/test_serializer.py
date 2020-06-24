2fd69e4562f9803e932edf90bdd4b5a36870dc41
Jonathan Barnoud <jonathan@barnoud.net>
2020-06-24 11:50:20 +0100

    Test that imd force is added at desiarialisation
    Former-commit-id: cbbb4ed853be3a3cf6f8865c7d535e3d3cf2198c

--- python-libraries/narupa-openmm/tests_narupa_openmm/test_serializer.py
+++ python-libraries/narupa-openmm/tests_narupa_openmm/test_serializer.py
@@ -9,13 +9,16 @@ Tests for :mod:`narupa.openmm.serializer`.
 from xml.dom.minidom import parseString
 import pytest
 
+import simtk.openmm as mm
+
 from narupa.openmm.serializer import (
     serialize_simulation,
     deserialize_simulation,
     ROOT_TAG,
 )
+from narupa.openmm.imd import create_imd_force
 
-from .simulation_utils import basic_simulation
+from .simulation_utils import basic_simulation, empty_imd_force
 
 
 @pytest.fixture
@@ -56,11 +59,12 @@ def add_extra_xml_tag(simulation_xml: str, extra_tag: str) -> str:
     return document.toprettyxml()
 
 
-def test_serialize_runs(basic_simulation_xml):
+@pytest.mark.parametrize('imd_force', (None, create_imd_force()))
+def test_serialize_runs(basic_simulation_xml, imd_force):
     """
     Test that a serialized simulation can be deserialized and run.
     """
-    simulation = deserialize_simulation(basic_simulation_xml)
+    simulation = deserialize_simulation(basic_simulation_xml, imd_force=imd_force)
     assert simulation.currentStep == 0
     simulation.step(5)
     assert simulation.currentStep == 5
@@ -85,3 +89,39 @@ def test_duplicate_section(basic_simulation_xml, section_to_duplicate):
     broken_xml = add_extra_xml_tag(basic_simulation_xml, section_to_duplicate)
     with pytest.raises(IOError):
         deserialize_simulation(broken_xml)
+
+
+def test_imd_force(basic_simulation_xml, empty_imd_force):
+    """
+    When deserializing a simulation and passing an imd force, the force is
+    added to the system.
+    """
+    simulation = deserialize_simulation(basic_simulation_xml, empty_imd_force)
+
+    system = simulation.system
+    all_forces = system.getForces()
+    # The wrapper for the C++ force is recreated at some point, so what
+    # system.getForces returns is a different python object as what we
+    # inputted initially, even though it references the same force under the
+    # hood. As I do not know how to access the common reference between the
+    # wrapper objects for the same force, I have to do some gymnastic.
+    putative_imd_forces = [
+        force for force in all_forces if (
+            isinstance(force, mm.CustomExternalForce)
+            and [
+                force.getPerParticleParameterName(i)
+                for i in range(force.getNumPerParticleParameters())
+            ] == ['fx', 'fy', 'fz']
+        )
+    ]
+    # In principle, there could be more than one force that matches the above
+    # description. In practice, though, it is not the case for now.
+    assert len(putative_imd_forces) == 1
+    force_obtained = putative_imd_forces[0]
+    force_added = empty_imd_force
+    # The forces are the same if by modifying one we also modify the other.
+    force_added.setParticleParameters(0, 0, (1.0, 2.0, 3.0))
+    parameters = force_obtained.getParticleParameters(0)
+    assert parameters == [0, (1.0, 2.0, 3.0)]
+
+
