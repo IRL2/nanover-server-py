53acf9588d9dadf8eb3de71002d327f0e2bae71a
Jonathan Barnoud <jonathan@barnoud.net>
2020-06-25 11:36:15 +0100

    Remove openmm server
    Will be superseeded by the runner.
    
    
    Former-commit-id: d258e337f3c4afcf2a2689dc3891cca8d7183802

--- python-libraries/narupa-openmm/src/narupa/openmm/__init__.py
+++ python-libraries/narupa-openmm/src/narupa/openmm/__init__.py
@@ -5,5 +5,4 @@ Interface between Narupa and OpenMM.
 """
 from .converter import openmm_to_frame_data
 from .narupareporter import NarupaReporter
-from .server import Server
 from .runner import Runner
