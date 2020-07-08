858be4c7f9e473b6f706e32ace6ce825a3095065
Jonathan Barnoud <jonathan@barnoud.net>
2020-07-08 15:26:32 +0100

    OpenMM runner exposes intervals with commands
    The OpenMM runner now exposes commands to get and set the frame and
    force intervals from a client. This does allow to tweak these parameters
    for a given simulation in order to reach a certain feel.
    
    
    Former-commit-id: b2290d512c5d1b19f731c9d4a5e2d21d9592905c

--- python-libraries/narupa-openmm/src/narupa/openmm/__init__.py
+++ python-libraries/narupa-openmm/src/narupa/openmm/__init__.py
@@ -5,4 +5,8 @@ Interface between Narupa and OpenMM.
 """
 from .converter import openmm_to_frame_data
 from .narupareporter import NarupaReporter
-from .runner import OpenMMRunner
+from .runner import (
+    OpenMMRunner,
+    GET_FRAME_INTERVAL_COMMAND_KEY, SET_FRAME_INTERVAL_COMMAND_KEY,
+    GET_FORCE_INTERVAL_COMMAND_KEY, SET_FORCE_INTERVAL_COMMAND_KEY,
+)
