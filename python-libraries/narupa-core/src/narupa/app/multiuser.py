2fea83979612c1907e67b128845810cd8c8289b0
mark <m.wonnacott@bristol.ac.uk>
2021-02-01 12:00:41 +0000

    unpack closure
    Former-commit-id: 44fada5608e833605347fe3d193e58df88710005

--- python-libraries/narupa-core/src/narupa/app/multiuser.py
+++ python-libraries/narupa-core/src/narupa/app/multiuser.py
@@ -5,6 +5,7 @@ Module for addon server behaviour specific to the NarupaIMD multiuser
 experience.
 """
 import math
+from functools import partial
 from narupa.core import NarupaServer
 from narupa.utilities.change_buffers import DictionaryChange
 
@@ -13,37 +14,41 @@ MULTIUSER_AVATAR_PREFIX = 'avatar.'
 MULTIUSER_ORIGIN_PREFIX = 'user-origin.'
 
 
+def _radially_orient_server(*, server, radius=1):
+    """
+    For each avatar present, add a suggested origin to the multiuser state
+    for the corresponding user id. Distributes the origins in a circle
+    around the true origin, each facing inwards, and each displaced radially
+    by the given radius.
+    """
+    # find relevant avatar ids
+    state = server.copy_state()
+    avatar_ids = [
+        key[len(MULTIUSER_AVATAR_PREFIX):]
+        for key in state
+        if key.startswith(MULTIUSER_AVATAR_PREFIX)
+    ]
+    # generate an origin for each avatar
+    count = len(avatar_ids)
+    angles = [i * math.pi * 2 / count for i in range(count)]
+    updates = {
+        MULTIUSER_ORIGIN_PREFIX + avatar_id: {
+            "position": [radius * math.cos(angle), 0, radius * math.sin(angle)],
+            "rotation": _angle_axis_quaternion_y(-angle),
+        }
+        for avatar_id, angle in zip(avatar_ids, angles)
+    }
+    server.update_state(None, DictionaryChange(updates))
+
+
 def add_multiuser_commands(server: NarupaServer):
     """
     Add server commands specific to the NarupaIMD multiuser experience.
     """
-    def radially_orient(radius=1):
-        """
-        For each avatar present, add a suggested origin to the multiuser state
-        for the corresponding user id. Distributes the origins in a circle
-        around the true origin, each facing inwards, and each displaced radially
-        by the given radius.
-        """
-        # find relevant avatar ids
-        state = server.copy_state()
-        avatar_ids = [
-            key[len(MULTIUSER_AVATAR_PREFIX):]
-            for key in state
-            if key.startswith(MULTIUSER_AVATAR_PREFIX)
-        ]
-        # generate an origin for each avatar
-        count = len(avatar_ids)
-        angles = [i * math.pi * 2 / count for i in range(count)]
-        updates = {
-            MULTIUSER_ORIGIN_PREFIX + avatar_id: {
-                "position": [radius * math.cos(angle), 0, radius * math.sin(angle)],
-                "rotation": _angle_axis_quaternion_y(-angle),
-            }
-            for avatar_id, angle in zip(avatar_ids, angles)
-        }
-        server.update_state(None, DictionaryChange(updates))
-
-    server.register_command(RADIAL_ORIENT_COMMAND_KEY, radially_orient)
+    server.register_command(
+        RADIAL_ORIENT_COMMAND_KEY,
+        partial(_radially_orient_server, server=server),
+    )
 
 
 def _angle_axis_quaternion_y(angle):
