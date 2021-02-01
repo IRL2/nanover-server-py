6c8dd4be0f920f17af66759c42f1278f177df2ae
mark <m.wonnacott@bristol.ac.uk>
2021-02-01 12:18:43 +0000

    use existing fixtures + constants
    Former-commit-id: a504720f28f2b83d15592842b57013ed4f93d67a

--- python-libraries/narupa-core/tests/app/test_multiuser.py
+++ python-libraries/narupa-core/tests/app/test_multiuser.py
@@ -1,37 +1,38 @@
 import time
 import numpy
 import pytest
-from narupa.app import NarupaApplicationServer, NarupaImdClient
 from narupa.app.multiuser import (
     RADIAL_ORIENT_COMMAND_KEY, MULTIUSER_ORIGIN_PREFIX,
 )
 from narupa.utilities.change_buffers import DictionaryChange
 
+from ..core.test_narupa_client_server_commands import client_server
+from ..core.test_narupa_client_server_state import IMMEDIATE_REPLY_WAIT_TIME
+
 
 @pytest.mark.parametrize('avatars', (0, 1, 4))
 @pytest.mark.parametrize('radius', (0, 1, 5))
-def test_radial_orient(avatars, radius):
+def test_radial_orient(client_server, avatars, radius):
     """
     Test that the radial orientation command creates the correct number of
     origin entries at the correct radius and spacing.
     """
-    with NarupaApplicationServer.basic_server(port=0) as server:
-        with NarupaImdClient.connect_to_single_server("localhost", server.port) as client:
-            client.subscribe_multiplayer(interval=0)
+    client, server = client_server
+    client.subscribe_multiplayer(interval=0)
 
-            update = DictionaryChange({
-                "avatar.test" + str(i): [] for i in range(avatars)
-            })
-            client.attempt_update_multiplayer_state(update)
-            client.run_command(RADIAL_ORIENT_COMMAND_KEY, radius=radius)
-            time.sleep(.001)
+    update = DictionaryChange({
+        "avatar.test" + str(i): [] for i in range(avatars)
+    })
+    client.attempt_update_multiplayer_state(update)
+    client.run_command(RADIAL_ORIENT_COMMAND_KEY, radius=radius)
+    time.sleep(IMMEDIATE_REPLY_WAIT_TIME)
 
-            origins = [
-                value
-                for key, value in client.latest_multiplayer_values.items()
-                if key.startswith(MULTIUSER_ORIGIN_PREFIX)
-            ]
-            positions = [origin["position"] for origin in origins]
+    origins = [
+        value
+        for key, value in client.latest_multiplayer_values.items()
+        if key.startswith(MULTIUSER_ORIGIN_PREFIX)
+    ]
+    positions = [origin["position"] for origin in origins]
 
-            assert len(origins) == avatars
-            assert all(numpy.linalg.norm(position) == radius for position in positions)
+    assert len(origins) == avatars
+    assert all(numpy.linalg.norm(position) == radius for position in positions)
