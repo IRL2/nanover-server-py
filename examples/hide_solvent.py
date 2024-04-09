9c5fff99f0c9457ed293e798e7ede8b3e030331f
Mark <mark@anuma.com>
2024-04-09 15:46:59 +0100

    first draft

---
+++ examples/hide_solvent.py
@@ -0,0 +1,21 @@
+from nanover.app import NanoverImdClient
+
+SOLVENT_RESIDUE_NAME = "HOH"
+
+with NanoverImdClient.autoconnect() as client:
+    client.subscribe_to_frames()
+    client.subscribe_multiplayer()
+    first_frame = client.wait_until_first_frame(check_interval=0.5, timeout=10)
+
+    print(f"Attempting to hide residue {SOLVENT_RESIDUE_NAME} (residues in frame: {', '.join(set(first_frame.residue_names))})")
+
+    particles = (
+        particle_index 
+        for particle_index, residue_index 
+        in enumerate(first_frame.particle_residues) 
+        if first_frame.residue_names[residue_index] == SOLVENT_RESIDUE_NAME
+    )
+
+    solvent = client.create_selection("solvent", particles)
+    with solvent.modify():
+        solvent.hide = True
