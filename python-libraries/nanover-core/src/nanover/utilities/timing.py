54c8f53126297a58a0c7cb855386880eefc5e9fc
Mark <mark@anuma.com>
2024-05-20 11:08:38 +0100

    change yield_interval to count intervals from the last yield target rather than the actual yield time (avoid compounding error)

--- python-libraries/nanover-core/src/nanover/utilities/timing.py
+++ python-libraries/nanover-core/src/nanover/utilities/timing.py
@@ -22,12 +22,12 @@ class VariableIntervalGenerator:
             self._interval = value
 
     def yield_interval(self):
-        prev_yield = time.perf_counter()
+        target = time.perf_counter()
+        prev_yield = target
         yield 0
         while True:
-            time_since_yield = time.perf_counter() - prev_yield
-            wait_duration = max(0.0, self.interval - time_since_yield)
-            time.sleep(wait_duration)
+            wait_mixed(target - time.perf_counter())
+            target += self.interval
             next_yield = time.perf_counter()
             yield next_yield - prev_yield
             prev_yield = next_yield
@@ -35,9 +35,46 @@ class VariableIntervalGenerator:
 
 def yield_interval(interval: float):
     """
-    Yield immediately and then every interval seconds, yielding the time in seconds that passed between yields.
+    Yield immediately and then roughly every interval seconds, yielding the time in seconds that passed between yields.
 
     :param interval: Number of seconds to ensure between yields
     :yield: Number of seconds since last yielding
     """
     return VariableIntervalGenerator(interval).yield_interval()
+
+
+def wait_sleep(seconds: float):
+    """
+    Do nothing for a period of time using time.sleep. Imprecise but doesn't use much CPU.
+    """
+    target = time.perf_counter() + seconds
+    duration = max(target - time.perf_counter(), 0)
+    if duration > 0:
+        time.sleep(duration)
+
+
+def wait_busy(seconds: float):
+    """
+    Do nothing for a period of time by tightly looping. Precise but uses much CPU.
+    """
+    target = time.perf_counter() + seconds
+    while time.perf_counter() < target:
+        pass
+
+
+def wait_mixed(seconds: float, sleep_error=.01):
+    """
+    Do nothing for a period of time by sleeping until close to the target then using CPU time.
+    """
+    target = time.perf_counter() + seconds
+    while True:
+        now = time.perf_counter()
+        duration = max(target - now, 0)
+        if duration == 0:
+            return
+        elif duration > sleep_error:
+            time.sleep(duration * .5)
+        else:
+            while time.perf_counter() < target:
+                pass
+
