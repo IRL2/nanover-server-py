7fbfc31518b0e88297e7912f806c3e55b25f4a08
candle <ragzouken@gmail.com>
2024-06-06 14:11:29 +0200

    more reliable test_context_managers

--- python-libraries/nanover-essd/tests/test_essd_client_server.py
+++ python-libraries/nanover-essd/tests/test_essd_client_server.py
@@ -144,6 +144,7 @@ def test_context_managers(service, properties_unique_id):
     service2 = ServiceHub(**properties_unique_id)
 
     run_with_server(service1)
+    time.sleep(0.1)  # give a small window for old servers to stop advertising
     run_with_server(service2, service1)
 
 
