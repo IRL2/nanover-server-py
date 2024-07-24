53e89715e7127bf72c1e61f5f9ff615534cdc3cf
candle <ragzouken@gmail.com>
2024-07-24 11:34:45 +0100

    messing with rich

--- python-libraries/nanover-omni/src/nanover/omni/rich.py
+++ python-libraries/nanover-omni/src/nanover/omni/rich.py
@@ -1,17 +1,28 @@
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical, VerticalScroll
+from textual.css.query import NoMatches
 from textual.reactive import reactive
-from textual.widgets import Button, Label
+from textual.screen import Screen
+from textual.widgets import Button, Label, Static
 
 from nanover.omni import OmniRunner
 
 
+class BSOD(Screen):
+    BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
+
+    def compose(self) -> ComposeResult:
+        yield Static(" Windows ", id="title")
+        yield Static(ERROR_TEXT)
+        yield Static("Press any key to continue [blink]_[/]", id="any-key")
+
+
 class OmniTextualApp(App):
     CSS_PATH = "rich.tcss"
-    BINDINGS = [("q", "quit", "Quit")]
+    BINDINGS = [("q", "quit", "Quit"), ("escape", "push_screen('bsod')", "BSOD")]
     TITLE = "NanoVer Omni Server"
 
-    simulation = reactive(None)
+    SCREENS = {"bsod": BSOD()}
 
     def __init__(self, omni: OmniRunner):
         super().__init__()
@@ -21,17 +32,24 @@ class OmniTextualApp(App):
         self.set_interval(1 / 50, self.update)
 
     def update(self):
+        if not self.screen.is_current:
+            return
+
         running = self.omni.simulation is not None
         paused = self.omni.paused
 
-        name = "None" if not running else self.omni.simulation.name
-        self.query_one("#status Label", Label).update(f"Running: {name}")
-        self.query_one("#play", Button).disabled = not paused
-        self.query_one("#pause", Button).disabled = paused
+        # TODO: don't know how to properly avoid calling this when elements absent...
+        try:
+            name = "None" if not running else self.omni.simulation.name
+            self.query_one("#status Label", Label).update(f"Running: {name}")
+            self.query_one("#play", Button).disabled = not paused
+            self.query_one("#pause", Button).disabled = paused
 
-        for button in self.query("#controls Button"):
-            button.disabled = not running
-        self.query_one("#quit").disabled = False
+            for button in self.query("#controls Button"):
+                button.disabled = not running
+            self.query_one("#quit").disabled = False
+        except NoMatches:
+            ...
 
     async def on_button_pressed(self, event: Button.Pressed):
         match event.button.id:
@@ -62,3 +80,16 @@ class OmniTextualApp(App):
                     yield Button("Step", id="step")
                     yield Button("Reset", id="reset")
                     yield Button("Quit", id="quit")
+
+
+ERROR_TEXT = """
+An error has occurred. To continue:
+
+Press Enter to return to Windows, or
+
+Press CTRL+ALT+DEL to restart your computer. If you do this,
+you will lose any unsaved information in all open applications.
+
+Error: 0E : 016F : BFF9B3D4
+"""
+
