22ae0a36dc50f7c648e7726cec5d1ff3b073ed00
candle <ragzouken@gmail.com>
2024-07-29 16:02:21 +0100

    add test for interactions working

--- python-libraries/nanover-omni/tests/test_openmm.py
+++ python-libraries/nanover-omni/tests/test_openmm.py
@@ -1,9 +1,11 @@
+import numpy as np
 import pytest
 
+from nanover.imd import ParticleInteraction
 from nanover.omni.openmm import OpenMMSimulation
 
 from common import app_server, ARGON_XML_PATH
-from nanover.openmm.imd import add_imd_force_to_system
+from nanover.openmm import serializer
 
 
 @pytest.fixture
@@ -12,6 +14,44 @@ def example_openmm(app_server):
     sim.load()
     sim.reset(app_server)
     yield sim
+
+
+def test_auto_force(app_server):
+    """
+    Test that interactions work if the imd force isn't added manually.
+    """
+    with open(ARGON_XML_PATH) as infile:
+        omm_sim = serializer.deserialize_simulation(infile)
+    omni_sim = OpenMMSimulation.from_simulation(omm_sim)
+    omni_sim.load()
+    omni_sim.reset(app_server)
+
+    def get_position():
+        positions = omni_sim.simulation.context.getState(
+            getPositions=True
+        ).getPositions(asNumpy=True)
+        return np.asarray(positions[0])
+
+    # add an interaction far to the right
+    prev_pos = get_position()
+    next_pos = list(prev_pos)
+    next_pos[0] += 100
+
+    interaction = ParticleInteraction(
+        interaction_type="constant",
+        position=next_pos,
+        particles=[0],
+        scale=10,
+    )
+    app_server.imd.insert_interaction("interaction.test", interaction)
+
+    # run some simulation steps
+    for _ in range(50):
+        omni_sim.advance_by_one_step()
+
+    # check the atom moved some way to the right
+    curr_pos = get_position()
+    assert curr_pos[0] - prev_pos[0] >= 1
 
 
 def test_step_interval(example_openmm):
