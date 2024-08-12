0ac55c568eade345acc27344140f7ff123042fbb
candle <ragzouken@gmail.com>
2024-08-12 11:44:36 +0100

    non-reporter omm

--- python-libraries/nanover-omni/src/nanover/omni/openmm.py
+++ python-libraries/nanover-omni/src/nanover/omni/openmm.py
@@ -3,14 +3,17 @@ from pathlib import Path
 from typing import Optional, Any
 
 from openmm.app import Simulation, StateDataReporter
+from openmm.unit import kilojoule_per_mole
 
 from nanover.app import NanoverImdApplication
-from nanover.openmm import serializer
+from nanover.openmm import serializer, openmm_to_frame_data
 from nanover.openmm.imd import (
     create_imd_force,
     NanoverImdReporter,
-    add_imd_force_to_system,
+    add_imd_force_to_system, ImdForceManager, OTHER_FORCE_GROUP_MASK,
 )
+from nanover.protocol.state import State
+from nanover.trajectory.frame_data import Array2Dfloat
 
 
 class OpenMMSimulation:
@@ -49,6 +52,9 @@ class OpenMMSimulation:
         self.reporter: Optional[NanoverImdReporter] = None
         self.verbose_reporter: Optional[StateDataReporter] = None
 
+        self.frame_index = 0
+        self.imd_force_manager: Optional[ImdForceManager] = None
+
     def load(self):
         if self.xml_path is None or self.simulation is not None:
             return
@@ -68,31 +74,76 @@ class OpenMMSimulation:
         self.simulation.context.loadCheckpoint(self.checkpoint)
 
         try:
-            self.simulation.reporters.remove(self.reporter)
             if self.verbose_reporter is not None:
                 self.simulation.reporters.remove(self.verbose_reporter)
         except ValueError:
             pass
 
-        self.reporter = NanoverImdReporter(
-            frame_interval=self.frame_interval,
-            force_interval=self.force_interval,
-            include_velocities=self.include_velocities,
-            include_forces=self.include_forces,
-            imd_force=self.imd_force,
-            imd_state=self.app_server.imd,
-            frame_publisher=self.app_server.frame_publisher,
-        )
-        self.simulation.reporters.append(self.reporter)
         if self.verbose_reporter is not None:
             self.simulation.reporters.append(self.verbose_reporter)
 
-    def advance_to_next_report(self):
-        assert self.simulation is not None
-        self.simulation.step(self.frame_interval)
+        self.imd_force_manager = ImdForceManager(self.app_server.imd, self.imd_force)
+
+        self.app_server.frame_publisher.send_frame(0, self.make_topology_frame(self.simulation))
+        self.frame_index = 1
 
     def advance_by_seconds(self, dt: float):
         self.advance_to_next_report()
 
     def advance_by_one_step(self):
         self.advance_to_next_report()
+
+    def advance_to_next_report(self):
+        assert self.simulation is not None
+        self.simulation.step(self.frame_interval)
+
+        step = self.simulation.currentStep
+        state = self.simulation.context.getState(
+            getPositions=True,
+            getForces=self.include_forces,
+            getVelocities=self.include_velocities,
+            getEnergy=True,
+        )
+
+        do_frame = step % self.frame_interval == 0
+        do_imd = step % self.force_interval == 0
+
+        positions = state.getPositions(asNumpy=True)
+        if do_frame:
+            frame_data = self.make_regular_frame(self.simulation, state, positions)
+            self.app_server.frame_publisher.send_frame(self.frame_index, frame_data)
+            self.frame_index += 1
+        if do_imd:
+            self.imd_force_manager.update_interactions(self.simulation, positions)
+
+    def make_topology_frame(self, simulation: Simulation):
+        state = simulation.context.getState(getPositions=True, getEnergy=True)
+        topology = simulation.topology
+        frame_data = openmm_to_frame_data(state=state, topology=topology)
+        return frame_data
+
+    def make_regular_frame(
+        self,
+        simulation: Simulation,
+        state: State,
+        positions: Array2Dfloat,
+    ):
+        frame_data = openmm_to_frame_data(
+            state=state,
+            topology=None,
+            include_positions=False,
+            include_velocities=self.include_velocities,
+            include_forces=self.include_forces,
+        )
+        frame_data.particle_positions = positions
+        self.imd_force_manager.add_to_frame_data(frame_data)
+
+        # Get the simulation state excluding the IMD force, and recalculate potential energy without it:
+        energy_no_imd = (
+            simulation.context.getState(getEnergy=True, groups=OTHER_FORCE_GROUP_MASK)
+            .getPotentialEnergy()
+            .value_in_unit(kilojoule_per_mole)
+        )
+        frame_data.potential_energy = energy_no_imd
+
+        return frame_data
