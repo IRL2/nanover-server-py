870cab1fbb27c09cf38a7ed36d003c9306ff659c
candle <ragzouken@gmail.com>
2024-10-01 13:52:19 +0200

    attempt updating setup.py to toml

--- python-libraries/nanover-ase/setup.py
+++ python-libraries/nanover-ase/setup.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from setuptools import find_namespace_packages
+from setuptools import setup
 
-setup(
-    name="nanover-ase",
-    version="0.1.0",
-    description="ASE integration for NanoVer",
-    author="Intangible Realities Lab",
-    author_email="m.oconnor@bristol.ac.uk",
-    url="https://gitlab.com/intangiblerealities/",
-    packages=find_namespace_packages("src", include="nanover.*"),
-    package_dir={"": "src"},
-    package_data={"": ["py.typed"]},
-    install_requires=(
-        "nanover",
-        "ase>=3.21",
-    ),
-    entry_points={
-        "console_scripts": ["nanover-omm-ase=nanover.ase.openmm.cli:main"],
-    },
-)
+setup()
