acdc8512dd2a63f6ffb87a2b1282bd2c5152b48c
Harry Stroud <75842225+hjstroud@users.noreply.github.com>
2024-10-10 17:20:59 +0200

    Calculate work done by user for OpenMM simulations (#253)
    * Initial commit, add class for calculating work done on the system and field in frame_data.py for work done
    
    * Define function to calculate and add work contribution
    
    * Correct sign error
    
    * Remove unused attribute
    
    * Change type of work_done to float
    
    * Fix docstrings error
    
    * Rephrase docstrings and comments in terms of previous- and on-step contributions rather than on- and next-step contributions to better match the code.
    
    * Add functionality to calculate the work done by the user to the OpenMMSimulation class (non-optionally)
    
    * Remove separate OpenMMSimulationWorkDone class
    
    * Reformat according to Black
    
    * Add OpenMM simulation utils to the Omni test directory
    
    * Change existing `test_step_interval` test to use an Argon simulation from the simulation definition in `openmm_simulation_utils.py` instead of the .xml file
    
    * Change existing `test_auto_force` test to use an Argon simulation without predefined imd force from the simulation definition in `openmm_simulation_utils.py` instead of the .xml file
    
    * Fix typos
    
    * Added fixture for single atom with constant force and initial draft of `test_work_done` (currently doesn't test work done)
    
    * Add _work_done_intermediate property to OpenMMSimulation object for greater clarity
    
    * Add tests to check that the calculated user work done comes out as expected and that the frame value matches the server value
    
    * Black formatting
    
    * import indirect fixture
    
    * Import single_atom_simulation to fix breaking tests
    
    ---------
    
    Co-authored-by: candle <ragzouken@gmail.com>

--- python-libraries/nanover-omni/tests/test_openmm.py
+++ python-libraries/nanover-omni/tests/test_openmm.py
@@ -1,28 +1,63 @@
 import numpy as np
 import pytest
 
+from nanover.openmm import imd
 from nanover.imd import ParticleInteraction
 from nanover.omni.openmm import OpenMMSimulation
+from nanover.app import NanoverImdApplication, NanoverImdClient
 
-from common import app_server, ARGON_XML_PATH
-from nanover.openmm import serializer
+from common import app_server
+
+from openmm_simulation_utils import (
+    basic_system,
+    basic_simulation,
+    basic_simulation_with_imd_force,
+    BASIC_SIMULATION_POSITIONS,
+    empty_imd_force,
+    assert_basic_simulation_topology,
+    single_atom_system,
+    single_atom_simulation,
+    single_atom_simulation_with_imd_force,
+    ARGON_SIMULATION_POSITION,
+    assert_single_atom_simulation_topology,
+)
 
 
 @pytest.fixture
-def example_openmm(app_server):
-    sim = OpenMMSimulation.from_xml_path(ARGON_XML_PATH)
+def example_openmm(app_server, single_atom_simulation):
+    sim = OpenMMSimulation.from_simulation(single_atom_simulation)
     sim.load()
     sim.reset(app_server)
     yield sim
 
 
-def test_auto_force(app_server):
+@pytest.fixture
+def single_atom_app_and_simulation_with_constant_force(
+    app_server, single_atom_simulation
+):
+    sim = OpenMMSimulation.from_simulation(single_atom_simulation)
+    sim.load()
+    sim.reset(app_server)
+
+    # Add a constant force interaction with the force positioned far
+    # from the origin (the initial position of the atom)
+    interaction = ParticleInteraction(
+        interaction_type="constant",
+        position=(0.0, 0.0, 1000.0),
+        particles=[0],
+        scale=1,
+    )
+
+    app_server.imd.insert_interaction("interaction.test", interaction)
+
+    yield app_server, sim
+
+
+def test_auto_force(app_server, single_atom_simulation):
     """
     Test that interactions work if the imd force isn't added manually.
     """
-    with open(ARGON_XML_PATH) as infile:
-        omm_sim = serializer.deserialize_simulation(infile)
-    omni_sim = OpenMMSimulation.from_simulation(omm_sim)
+    omni_sim = OpenMMSimulation.from_simulation(single_atom_simulation)
     omni_sim.load()
     omni_sim.reset(app_server)
 
@@ -63,3 +98,44 @@ def test_step_interval(example_openmm):
             example_openmm.simulation.currentStep == i * example_openmm.frame_interval
         )
         example_openmm.advance_by_one_step()
+
+
+def test_work_done_server(single_atom_app_and_simulation_with_constant_force):
+    """
+    Test that the calculated user work done on a single atom system gives the
+    expected numerical result within the code.
+    """
+
+    # For a simulation with a frame interval of 5 simulation steps and a simulation
+    # step size of 2 fs, using a constant force to accelerate the atom at 1 nm ps-1
+    # (for Ar this is a force of 40 kJ mol-1 nm-1), after 100 steps the work done on
+    # an Argon atom should be 20.0 kJ mol-1 analytically. Allowing for numerical error,
+    # the expected value should be slightly greater than 20.0 kJ mol-1
+
+    app, sim = single_atom_app_and_simulation_with_constant_force
+
+    # Add step to account for zeroth (topology) frame where force is not applied
+    for _ in range(101):
+        sim.advance_to_next_report()
+
+    # Check that 1.01 ps have passed (and hence that force has been applied for 1 ps)
+    assert sim.simulation.context.getTime()._value == pytest.approx(1.01, abs=10e-12)
+    assert sim.work_done == pytest.approx(20.0, abs=0.05)
+
+
+def test_work_done_frame(single_atom_app_and_simulation_with_constant_force):
+    """
+    Test that the calculated user work done on a single atom system that appears
+    in the frame is equal to the user work done as calculated in the OpenMMSimulation.
+    """
+    app, sim = single_atom_app_and_simulation_with_constant_force
+
+    for _ in range(11):
+        sim.advance_to_next_report()
+
+    with NanoverImdClient.connect_to_single_server(
+        port=app.port, address="localhost"
+    ) as client:
+        client.subscribe_to_frames()
+        client.wait_until_first_frame()
+        assert client.current_frame.user_work_done == sim.work_done
