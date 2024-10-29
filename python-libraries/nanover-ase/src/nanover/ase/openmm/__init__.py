1890d853d3f0b691834011715975fd0b8968a7b8
candle <ragzouken@gmail.com>
2024-10-29 15:49:10 +0100

    refactor!: remove old ASEOpenMMRunner

--- python-libraries/nanover-ase/src/nanover/ase/openmm/__init__.py
+++ python-libraries/nanover-ase/src/nanover/ase/openmm/__init__.py
@@ -1,5 +1,4 @@
 from .calculator import OpenMMCalculator
-from .runner import ASEOpenMMRunner
 
 """
 Module containing ASE calculators and runners for running NanoVer compatible OpenMM simulations with ASE.
