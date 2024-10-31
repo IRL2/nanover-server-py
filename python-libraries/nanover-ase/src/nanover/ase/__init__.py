e90aadf9353898c6dae8258058bfbbee9882e9b5
candle <ragzouken@gmail.com>
2024-10-31 11:05:56 +0100

    refactor!: remove obsolete ase and tests

--- python-libraries/nanover-ase/src/nanover/ase/__init__.py
+++ python-libraries/nanover-ase/src/nanover/ase/__init__.py
@@ -4,5 +4,4 @@ Interface between NanoVer and ASE.
 
 from .converter import ase_to_frame_data
 from .frame_adaptor import send_ase_frame
-from .imd import NanoverASEDynamics
 from .trajectory_logger import TrajectoryLogger
