c84449353b7e9662cebbf3e6d73564c299ebf1ad
Harry Stroud <harry.stroud@usc.es>
2024-11-22 15:08:02 +0100

    Create separate functions for adding the system forces and velocities to the frame data for ASE simulations, update description of `add_ase_state_to_frame_data`

--- python-libraries/nanover-ase/src/nanover/ase/converter.py
+++ python-libraries/nanover-ase/src/nanover/ase/converter.py
@@ -114,15 +114,9 @@ def ase_to_frame_data(
     if box_vectors:
         add_ase_box_vectors_to_frame_data(data, ase_atoms)
     if include_velocities:
-        data.particle_velocities = ase_atoms.get_velocities() * (
-            ANG_TO_NM / ASE_TIME_UNIT_TO_PS
-        )
+        add_ase_velocities_to_frame_data(data, ase_atoms)
     if include_forces:
-        data.particle_forces_system = ase_atoms.get_forces() * (EV_TO_KJMOL / ANG_TO_NM)
-        if isinstance(ase_atoms.calc, ImdCalculator):
-            data.particle_forces_system -= ase_atoms.calc.results[
-                "interactive_forces"
-            ] * (EV_TO_KJMOL / ANG_TO_NM)
+        add_ase_forces_to_frame_data(data, ase_atoms)
 
     return data
 
@@ -202,6 +196,35 @@ def add_ase_positions_to_frame_data(data: FrameData, positions: npt.NDArray):
     data.particle_positions = positions * ANG_TO_NM
 
 
+def add_ase_velocities_to_frame_data(data: FrameData, ase_atoms: Atoms):
+    """
+    Adds ASE velocities to the frame data, converting to nanometers per picosecond.
+
+    :param data: :class:`FrameData` to add atom velocities to.
+    :param ase_atoms: ASE :class:`Atoms` to add particle positions to.
+    """
+    data.particle_velocities = ase_atoms.get_velocities() * (
+            ANG_TO_NM / ASE_TIME_UNIT_TO_PS
+    )
+
+
+def add_ase_forces_to_frame_data(data: FrameData, ase_atoms: Atoms):
+    """
+    Adds ASE forces to the frame data, converting to kJ mol-1 per nanometer. If the ASE
+    calculator is an ImdCalculator, removes the iMD forces from the ASE forces to deliver
+    the system forces (iMD forces delivered separately elsewhere).
+
+    :param data: :class:`FrameData` to add atom forces to.
+    :param ase_atoms: ASE :class:`Atoms` to add particle positions to.
+    """
+
+    data.particle_forces_system = ase_atoms.get_forces() * (EV_TO_KJMOL / ANG_TO_NM)
+    if isinstance(ase_atoms.calc, ImdCalculator):
+        data.particle_forces_system -= ase_atoms.calc.results[
+                                           "interactive_forces"
+                                       ] * (EV_TO_KJMOL / ANG_TO_NM)
+
+
 def add_ase_box_vectors_to_frame_data(data: FrameData, ase_atoms: Atoms):
     """
     Adds the periodic box vectors from the given ASE :class:`Atoms`
@@ -256,7 +279,11 @@ def add_ase_topology_to_frame_data(
 def add_ase_state_to_frame_data(frame_data: FrameData, ase_atoms: Atoms):
     """
     Adds simulation state information to the frame,
-    consisting of the potential energy and kinetic energy.
+    consisting of the potential energy and kinetic energy of the
+    molecular system. If the ASE calculator is an ImdCalculator,
+    removes the iMD energy from the ASE potential energy to deliver
+    the system potential energy (iMD energy delivered separately
+    elsewhere).
 
     :param frame_data: Frame data to add ASE state information to.
     :param ase_atoms: The ASE atoms from which to extract state information.
