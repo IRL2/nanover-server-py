1e4de00880495d65592c9d418b50922ac947cd40
mark wonnacott <ragzouken@gmail.com>
2024-12-17 12:26:25 +0100

    Create missing directories for recording and print out exceptions during recording.

--- python-libraries/nanover-omni/src/nanover/omni/record.py
+++ python-libraries/nanover-omni/src/nanover/omni/record.py
@@ -1,10 +1,15 @@
+import traceback
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
 
 import grpc
 
 from nanover.protocol.state import StateStub, SubscribeStateUpdatesRequest
 from nanover.protocol.trajectory import TrajectoryServiceStub, GetFrameRequest
 from nanover.recording.writing import record_messages
+
+
+from threading import Lock
 
 
 def record_from_server(address, trajectory_file, state_file):
@@ -15,15 +20,32 @@ def record_from_server(address, trajectory_file, state_file):
     :param state_file: File to write state updates to
     :return:
     """
+    print_lock = Lock()
+
+    def error_handler(f):
+        e = f.exception()
+
+        if e is None:
+            return
+
+        with print_lock:
+            traceback.print_exc()
+
     channel = grpc.insecure_channel(address)
+
     executor = ThreadPoolExecutor(max_workers=2)
-    executor.submit(record_trajectory, trajectory_file, channel)
-    executor.submit(record_state, state_file, channel)
+    executor.submit(record_trajectory, trajectory_file, channel).add_done_callback(
+        error_handler
+    )
+    executor.submit(record_state, state_file, channel).add_done_callback(error_handler)
     return executor, channel
 
 
 def record_trajectory(path, channel):
-    with open(path, "wb") as io:
+    path = Path(path)
+    path.parent.mkdir(exist_ok=True, parents=True)
+
+    with path.open("wb") as io:
         stub = TrajectoryServiceStub(channel)
         request = GetFrameRequest()
         stream = stub.SubscribeLatestFrames(request)
@@ -31,7 +53,10 @@ def record_trajectory(path, channel):
 
 
 def record_state(path, channel):
-    with open(path, "wb") as io:
+    path = Path(path)
+    path.parent.mkdir(exist_ok=True, parents=True)
+
+    with path.open("wb") as io:
         stub = StateStub(channel)
         request = SubscribeStateUpdatesRequest()
         stream = stub.SubscribeStateUpdates(request)
