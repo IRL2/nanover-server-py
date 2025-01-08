7370c2b2d7c231f96ce36a92284252dc0431fe77
mark wonnacott <ragzouken@gmail.com>
2025-01-08 17:08:04 +0100

    Remove dead class and update docstring.

--- python-libraries/nanover-openmm/src/nanover/openmm/imd.py
+++ python-libraries/nanover-openmm/src/nanover/openmm/imd.py
@@ -1,71 +1,21 @@
 """
-Link NanoVer's user forces to an OpenMM simulation.
-
-The iMD is hooked to the OpenMM simulation in two places. A
-:class:`~openmm.CustomExternalForce` needs to be added in the system; it
-has the components of the iMD force for each atom as a per-atom parameter. A
-reporter periodically updates these parameters based on the imd service, and
-updates the simulation context.
-
-The custom force can be setup using :func:`create_imd_force` and
-:func:`populate_imd_force`, or using :func:`add_imd_force_to_system` that combines
-the two previous functions. When a simulation is created using
-:func:`nanover.openmm.serializer.deserialize_simulation`, the imd force must be
-already present, or must be added by passing it with the ``imd_force``
-parameter.
-
-The reporter is :class:`NanoverImdReporter` and both sends the frames and
-receives the interactions. It can be use instead of
-:class:`nanover.openmm.NanoverReporter` that only sends the frames.
-
-.. code:: python
-
-    from nanover.app import NanoverImdApplication
-    from nanover.openmm.serializer import deserialize_simulation
-    from nanover.openmm.imd import NanoverImdReporter, create_imd_force
-
-    # Setup the NanoVer application server
-    # The server is accessible using autoconnect.
-    with NanoverImdApplication.basic_server() as app:
-
-        # Create the imd force and a simulation that includes it.
-        imd_force = create_imd_force()
-        with open('simulation.xml') as infile:
-            simulation = deserialize_simulation(infile.read(), imd_force=imd_force)
-
-        # Setup the reporter that does the translation between NanoVer and OpenMM
-        reporter = NanoverImdReporter(
-            frame_interval=5,
-            force_interval=10,
-            imd_force=imd_force,
-            imd_service=app.imd,
-            frame_publisher=app.frame_publisher,
-        )
-        simulation.reporters.append(reporter)
-
-        # Run the simulation
-        while True:
-            simulation.run(10)
-
+Manage an OpenMM CustomExternalForce in conjunction with NanoVer IMD
 """
 
-from typing import Dict, Set, Optional, NamedTuple, Tuple
+from typing import Dict, Set, Optional, Tuple
 import itertools
 
 import numpy as np
 import numpy.typing as npt
 
-from openmm import State, CustomExternalForce, System, Context
+from openmm import CustomExternalForce, System, Context
 from openmm import unit
 from openmm.app import Simulation
-from openmm.unit import kilojoule_per_mole
 
 from nanover.imd.imd_force import calculate_imd_force, get_sparse_forces
 from nanover.imd import ImdStateWrapper
-from nanover.trajectory.frame_publisher import FramePublisher
 from nanover.imd.particle_interaction import ParticleInteraction
-from .converter import openmm_to_frame_data
-from nanover.trajectory.frame_data import Array2Dfloat, FrameData
+from nanover.trajectory.frame_data import FrameData
 
 IMD_FORCE_EXPRESSION = "-fx * x - fy * y - fz * z"
 
@@ -73,122 +23,6 @@ ALL_FORCES_GROUP_MASK = 0xFFFFFFFF
 IMD_FORCES_GROUP = 31
 IMD_FORCES_GROUP_MASK = 1 << IMD_FORCES_GROUP
 NON_IMD_FORCES_GROUP_MASK = ALL_FORCES_GROUP_MASK ^ IMD_FORCES_GROUP_MASK
-
-
-class NextReport(NamedTuple):
-    steps: int
-    include_positions: bool
-    include_velocities: bool
-    include_forces: bool
-    include_energies: bool
-    wrap_positions: bool
-
-
-class NanoverImdReporter:
-    frame_interval: int
-    force_interval: int
-    include_velocities: bool
-    include_forces: bool
-    imd_force: CustomExternalForce
-    frame_publisher: FramePublisher
-    _frame_index: int
-
-    def __init__(
-        self,
-        frame_interval: int,
-        force_interval: int,
-        include_velocities: bool,
-        include_forces: bool,
-        imd_force: CustomExternalForce,
-        imd_state: ImdStateWrapper,
-        frame_publisher: FramePublisher,
-    ):
-        self.frame_interval = frame_interval
-        self.force_interval = force_interval
-        self.include_velocities = include_velocities
-        self.include_forces = include_forces
-        self.imd_force = imd_force
-        self.frame_publisher = frame_publisher
-
-        self.imd_force_manager = ImdForceManager(imd_state, imd_force)
-
-        self._did_first_frame = False
-        self._frame_index = 1
-
-    # The name of the method is part of the OpenMM API. It cannot be made to
-    # conform PEP8.
-    # noinspection PyPep8Naming
-    def describeNextReport(self, simulation: Simulation) -> NextReport:
-        """
-        Called by OpenMM. Indicates when the next report is due and what type
-        of data it requires.
-        """
-        if not self._did_first_frame:
-            self._did_first_frame = True
-            self.frame_publisher.send_frame(0, self.make_topology_frame(simulation))
-
-        force_steps = self.force_interval - simulation.currentStep % self.force_interval
-        frame_steps = self.frame_interval - simulation.currentStep % self.frame_interval
-        steps = min(force_steps, frame_steps)
-
-        return NextReport(
-            steps=steps,
-            include_positions=True,
-            include_velocities=self.include_velocities,
-            include_forces=self.include_forces,
-            include_energies=True,
-            wrap_positions=False,
-        )
-
-    def report(self, simulation: Simulation, state: State) -> None:
-        """
-        Called by OpenMM.
-        """
-        positions = None
-        if simulation.currentStep % self.force_interval == 0:
-            positions = state.getPositions(asNumpy=True)
-            self.imd_force_manager.update_interactions(simulation, positions)
-        if simulation.currentStep % self.frame_interval == 0:
-            frame_data = self.make_regular_frame(simulation, state, positions)
-            self.frame_publisher.send_frame(self._frame_index, frame_data)
-            self._frame_index += 1
-
-    def make_topology_frame(self, simulation: Simulation):
-        state = simulation.context.getState(getPositions=True, getEnergy=True)
-        topology = simulation.topology
-        frame_data = openmm_to_frame_data(state=state, topology=topology)
-        return frame_data
-
-    def make_regular_frame(
-        self,
-        simulation: Simulation,
-        state: State,
-        positions: Optional[Array2Dfloat] = None,
-    ):
-        if positions is None:
-            positions = state.getPositions(asNumpy=True)
-
-        frame_data = openmm_to_frame_data(
-            state=state,
-            topology=None,
-            include_positions=False,
-            include_velocities=self.include_velocities,
-            include_forces=self.include_forces,
-        )
-        frame_data.particle_positions = positions
-        self.imd_force_manager.add_to_frame_data(frame_data)
-
-        # Get the simulation state excluding the IMD force, and recalculate potential energy without it:
-        energy_no_imd = (
-            simulation.context.getState(
-                getEnergy=True, groups=NON_IMD_FORCES_GROUP_MASK
-            )
-            .getPotentialEnergy()
-            .value_in_unit(kilojoule_per_mole)
-        )
-        frame_data.potential_energy = energy_no_imd
-
-        return frame_data
 
 
 class ImdForceManager:
