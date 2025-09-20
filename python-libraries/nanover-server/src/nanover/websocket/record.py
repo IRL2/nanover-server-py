32e8a2e90d76bfe1cbf6614b1339a0dd478acddb
candle <ragzouken@gmail.com>
2025-09-20 18:33:37 +0200

    cleanup

--- python-libraries/nanover-server/src/nanover/websocket/record.py
+++ python-libraries/nanover-server/src/nanover/websocket/record.py
@@ -1,15 +1,11 @@
 import traceback
 from concurrent.futures import ThreadPoolExecutor
-from dataclasses import dataclass
 from time import perf_counter_ns
-from zipfile import ZipFile
 
 import msgpack
-from websockets import ConnectionClosed
 from websockets.sync.client import connect, ClientConnection
 
 from nanover.omni import OmniRunner
-from nanover.recording.utilities import iter_recording_max
 from nanover.recording.writing import write_entry, write_header
 from nanover.state.state_service import dictionary_change_to_state_update
 from nanover.trajectory.frame_data import FRAME_INDEX
@@ -18,7 +14,7 @@ from nanover.websocket.client.app_client import get_websocket_address_from_app_s
 from nanover.websocket.client.base_client import MAX_MESSAGE_SIZE
 from nanover.websocket.convert import (
     convert_dict_frame_to_grpc_frame,
-    unpack_dict_frame, pack_dict_frame, convert_grpc_frame_to_dict_frame,
+    unpack_dict_frame,
 )
 from nanover.protocol.trajectory import GetFrameResponse
 
@@ -37,87 +33,6 @@ def record_from_runner(runner: OmniRunner, trajectory_file, state_file):
         trajectory_file=trajectory_file,
         state_file=state_file,
     )
-
-
-@dataclass(kw_only=True)
-class RecordingIndexMessageEntry:
-    offset: int
-    length: int
-    timestamp: int
-
-
-def convert_recording(out_path, *, traj, state):
-    for event in iter_recording_max(traj=traj, state=state):
-        if event.next_frame_event is not None:
-            frame_response = event.next_frame_event.message
-            frame = convert_grpc_frame_to_dict_frame(frame_response.frame)
-            frame[FRAME_INDEX] = frame_response.index
-            message = {"frame": pack_dict_frame(frame)}
-
-
-def read_recording_v2(in_path):
-    with ZipFile(in_path) as archive:
-        with archive.open("index.msgpack") as index_file:
-            index = msgpack.unpackb(index_file.read())
-        with archive.open("received.msgpack") as received_file:
-            print(index[50])
-
-            offset = index[50]["offset"]
-            length = index[51]["offset"] - offset
-
-            received_file.seek(offset)
-            data = received_file.read(length)
-
-            message = msgpack.unpackb(data)
-            print(message)
-
-
-def record_from_server_v2(address, out_path):
-    start_time = perf_counter_ns()
-
-    def get_timestamp():
-        return int((perf_counter_ns() - start_time) / 1000)
-
-    def listen(websocket: ClientConnection):
-        with ZipFile(out_path, "w") as archive:
-            entries: list[RecordingIndexMessageEntry] = []
-
-            with archive.open("received.msgpack", "w") as messages_file:
-                offset = 0
-
-                try:
-                    for data in websocket:
-                        timestamp = get_timestamp()
-                        entries.append(
-                            RecordingIndexMessageEntry(
-                                offset=offset,
-                                length=len(data),
-                                timestamp=timestamp,
-                            )
-                        )
-                        messages_file.write(data)
-                        offset += len(data)
-                except ConnectionClosed:
-                    pass
-                except Exception:
-                    traceback.print_exc()
-                    raise
-                finally:
-                    websocket.close()
-
-            with archive.open("index.msgpack", "w") as index_file:
-                data = [{
-                    "offset": entry.offset,
-                    "timestamp": entry.timestamp,
-                    "length": entry.length,
-                } for entry in entries]
-                index_file.write(msgpack.packb(data))
-
-    websocket = connect(address, max_size=MAX_MESSAGE_SIZE)
-    executor = ThreadPoolExecutor(max_workers=1)
-    executor.submit(listen, websocket)
-
-    return executor, websocket
 
 
 def record_from_server(address, trajectory_file, state_file):
---
+++ python-libraries/nanover-server/src/nanover/websocket/record2.py
@@ -0,0 +1,113 @@
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass, asdict
+from time import perf_counter_ns
+from typing import IO, Iterable
+from zipfile import ZipFile
+
+import msgpack
+from websockets import ConnectionClosed
+from websockets.sync.client import connect, ClientConnection
+
+from nanover.recording.utilities import iter_recording_max
+from nanover.state.state_service import state_update_to_dictionary_change
+from nanover.trajectory.frame_data import FRAME_INDEX
+from nanover.websocket.client.base_client import MAX_MESSAGE_SIZE
+from nanover.websocket.convert import convert_grpc_frame_to_dict_frame, pack_dict_frame
+
+
+@dataclass(kw_only=True)
+class MessageEvent:
+    timestamp: int
+    data: bytes
+
+
+@dataclass(kw_only=True)
+class RecordingIndexEntry:
+    offset: int
+    length: int
+    timestamp: int
+
+    def read_from(self, io: IO[bytes]):
+        io.seek(self.offset)
+        return io.read(self.length)
+
+
+def read_recording_v2(in_path):
+    with ZipFile(in_path) as archive:
+        with archive.open("index.msgpack") as index_file:
+            index = msgpack.unpackb(index_file.read())
+        with archive.open("received.msgpack") as received_file:
+            print(index[50])
+
+            entry = RecordingIndexEntry(**index[50])
+            data = entry.read_from(received_file)
+
+            message = msgpack.unpackb(data)
+            print(message)
+
+
+def convert_old_recording(out_path, *, traj, state):
+    record_messages(out_path, message_events_from_old_recording(traj=traj, state=state))
+
+
+def record_from_server(address, out_path):
+    websocket = connect(address, max_size=MAX_MESSAGE_SIZE)
+    executor = ThreadPoolExecutor(max_workers=1)
+    executor.submit(record_messages, out_path, message_events_from_websocket(websocket))
+
+    return executor, websocket
+
+
+def message_events_from_websocket(websocket: ClientConnection):
+    start_time = perf_counter_ns()
+
+    def get_timestamp():
+        return int((perf_counter_ns() - start_time) / 1000)
+
+    try:
+        for data in websocket:
+            if isinstance(data, bytes):
+                yield MessageEvent(timestamp=get_timestamp(), data=data)
+    except ConnectionClosed:
+        pass
+    finally:
+        websocket.close()
+
+
+def message_events_from_old_recording(*, traj, state):
+    for event in iter_recording_max(traj=traj, state=state):
+        if event.next_frame_event is not None:
+            frame_response = event.next_frame_event.message
+            frame = convert_grpc_frame_to_dict_frame(frame_response.frame)
+            frame[FRAME_INDEX] = frame_response.index
+            message = {"frame": pack_dict_frame(frame)}
+            data = msgpack.packb(message)
+            yield MessageEvent(timestamp=event.timestamp, data=data)
+        if event.next_state_event is not None:
+            change = state_update_to_dictionary_change(event.next_state_event.message)
+            message = {
+                "state": {"updates": change.updates, "removals": list(change.removals)}
+            }
+            data = msgpack.packb(message)
+            yield MessageEvent(timestamp=event.timestamp, data=data)
+
+
+def record_messages(out_path: str, messages: Iterable[MessageEvent]):
+    with ZipFile(out_path, "w") as archive:
+        entries: list[RecordingIndexEntry] = []
+        try:
+            with archive.open("received.msgpack", "w") as messages_file:
+                offset = 0
+                for event in messages:
+                    entry = RecordingIndexEntry(
+                        offset=offset,
+                        length=len(event.data),
+                        timestamp=event.timestamp,
+                    )
+                    entries.append(entry)
+                    messages_file.write(event.data)
+                    offset += entry.length
+        finally:
+            with archive.open("index.msgpack", "w") as index_file:
+                data = [asdict(entry) for entry in entries]
+                index_file.write(msgpack.packb(data))
