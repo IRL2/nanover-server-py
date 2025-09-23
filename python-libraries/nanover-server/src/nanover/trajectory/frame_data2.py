e6d3f2a01498805d48a2186b150c5c20b1816659
candle <ragzouken@gmail.com>
2025-09-23 21:42:36 +0200

    test

--- python-libraries/nanover-server/src/nanover/trajectory/frame_data2.py
+++ python-libraries/nanover-server/src/nanover/trajectory/frame_data2.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any, no_type_check
+from typing import Any
 
 import numpy as np
 import numpy.typing as npt
@@ -57,6 +57,10 @@ class _Shortcut:
         return property(fget=get, fset=set)
 
 
+def _shortcut(key: str) -> Any:
+    return _Shortcut(key=key)
+
+
 class _FrameDataMeta(type):
     """
     Metaclass that adds shortcuts to the :class:`FrameData` class.
@@ -77,46 +81,45 @@ class _FrameDataMeta(type):
         cls._shortcuts = shortcuts
 
 
-@no_type_check
 class FrameData(metaclass=_FrameDataMeta):
     _shortcuts: dict[str, _Shortcut]
 
     def __init__(self, frame_dict: FrameDict):
         self.frame_dict = frame_dict
 
-    box_vectors: FloatArray = _Shortcut(key=BOX_VECTORS)
+    box_vectors: FloatArray = _shortcut(key=BOX_VECTORS)
 
-    bond_pairs: IndexArray = _Shortcut(key=BOND_PAIRS)
-    bond_orders: EnumArray = _Shortcut(key=BOND_ORDERS)
+    bond_pairs: IndexArray = _shortcut(key=BOND_PAIRS)
+    bond_orders: EnumArray = _shortcut(key=BOND_ORDERS)
 
-    particle_positions: FloatArray = _Shortcut(key=PARTICLE_POSITIONS)
-    particle_velocities: FloatArray = _Shortcut(key=PARTICLE_VELOCITIES)
-    particle_forces: FloatArray = _Shortcut(key=PARTICLE_FORCES)
-    particle_forces_system: FloatArray = _Shortcut(key=PARTICLE_FORCES_SYSTEM)
-    particle_elements: EnumArray = _Shortcut(key=PARTICLE_ELEMENTS)
-    particle_names: StringArray = _Shortcut(key=PARTICLE_NAMES)
-    particle_residues: IndexArray = _Shortcut(key=PARTICLE_RESIDUES)
-    particle_count: int = _Shortcut(key=PARTICLE_COUNT)
+    particle_positions: FloatArray = _shortcut(key=PARTICLE_POSITIONS)
+    particle_velocities: FloatArray = _shortcut(key=PARTICLE_VELOCITIES)
+    particle_forces: FloatArray = _shortcut(key=PARTICLE_FORCES)
+    particle_forces_system: FloatArray = _shortcut(key=PARTICLE_FORCES_SYSTEM)
+    particle_elements: EnumArray = _shortcut(key=PARTICLE_ELEMENTS)
+    particle_names: StringArray = _shortcut(key=PARTICLE_NAMES)
+    particle_residues: IndexArray = _shortcut(key=PARTICLE_RESIDUES)
+    particle_count: int = _shortcut(key=PARTICLE_COUNT)
 
-    residue_names: StringArray = _Shortcut(key=RESIDUE_NAMES)
-    residue_ids: StringArray = _Shortcut(key=RESIDUE_IDS)
-    residue_chains: IndexArray = _Shortcut(key=RESIDUE_CHAINS)
-    residue_count: int = _Shortcut(key=RESIDUE_COUNT)
+    residue_names: StringArray = _shortcut(key=RESIDUE_NAMES)
+    residue_ids: StringArray = _shortcut(key=RESIDUE_IDS)
+    residue_chains: IndexArray = _shortcut(key=RESIDUE_CHAINS)
+    residue_count: int = _shortcut(key=RESIDUE_COUNT)
 
-    chain_names: StringArray = _Shortcut(key=CHAIN_NAMES)
-    chain_count: int = _Shortcut(key=CHAIN_COUNT)
+    chain_names: StringArray = _shortcut(key=CHAIN_NAMES)
+    chain_count: int = _shortcut(key=CHAIN_COUNT)
 
-    kinetic_energy: float = _Shortcut(key=KINETIC_ENERGY)
-    potential_energy: float = _Shortcut(key=POTENTIAL_ENERGY)
-    system_temperature: float = _Shortcut(key=SYSTEM_TEMPERATURE)
+    kinetic_energy: float = _shortcut(key=KINETIC_ENERGY)
+    potential_energy: float = _shortcut(key=POTENTIAL_ENERGY)
+    system_temperature: float = _shortcut(key=SYSTEM_TEMPERATURE)
 
-    user_energy: float = _Shortcut(key=USER_ENERGY)
-    user_forces_sparse: FloatArray = _Shortcut(key=USER_FORCES_SPARSE)
-    user_forces_index: IndexArray = _Shortcut(key=USER_FORCES_INDEX)
-    user_work_done: float = _Shortcut(key=USER_WORK_DONE)
+    user_energy: float = _shortcut(key=USER_ENERGY)
+    user_forces_sparse: FloatArray = _shortcut(key=USER_FORCES_SPARSE)
+    user_forces_index: IndexArray = _shortcut(key=USER_FORCES_INDEX)
+    user_work_done: float = _shortcut(key=USER_WORK_DONE)
 
-    simulation_time: float = _Shortcut(key=SIMULATION_TIME)
-    simulation_counter: float = _Shortcut(key=SIMULATION_COUNTER)
-    simulation_exception: float = _Shortcut(key=SIMULATION_EXCEPTION)
+    simulation_time: float = _shortcut(key=SIMULATION_TIME)
+    simulation_counter: float = _shortcut(key=SIMULATION_COUNTER)
+    simulation_exception: float = _shortcut(key=SIMULATION_EXCEPTION)
 
-    server_timestamp: float = _Shortcut(key=SERVER_TIMESTAMP)
+    server_timestamp: float = _shortcut(key=SERVER_TIMESTAMP)
