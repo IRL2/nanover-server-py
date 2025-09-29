5c7789e193bd2bcd391e4549ffbb0ba36653b316
mark wonnacott <ragzouken@gmail.com>
2025-09-29 16:11:28 +0200

    writer cleanup + fix split cli

--- python-libraries/nanover-server/src/nanover/recording/writing.py
+++ python-libraries/nanover-server/src/nanover/recording/writing.py
@@ -1,6 +1,7 @@
 from dataclasses import asdict
 from io import BytesIO
-from typing import Iterable, Any
+from os import PathLike
+from typing import Iterable, Any, BinaryIO
 from zipfile import ZipFile
 
 import msgpack
@@ -14,30 +15,53 @@ from nanover.recording.reading import (
 from nanover.trajectory.keys import FRAME_INDEX
 
 
+class NanoverRecordingWriter:
+    @classmethod
+    def from_path(cls, path: PathLike[str] | str):
+        return cls(path)
+
+    def __init__(self, outfile: BinaryIO | PathLike[str] | str):
+        self.offset = 0
+        self.archive = ZipFile(outfile, "w")
+        self.messages_file = self.archive.open(
+            RECORDING_MESSAGES_FILENAME, "w", force_zip64=True
+        )
+        self.index: list[RecordingIndexEntry] = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def close(self):
+        self.messages_file.close()
+        self.write_index()
+        self.archive.close()
+
+    def write_message_event(self, event: MessageEvent):
+        data = msgpack.packb(event.message)
+        metadata = generate_metadata(event.message)
+        metadata["timestamp"] = event.timestamp
+        entry = RecordingIndexEntry(
+            offset=self.offset,
+            length=len(data),
+            metadata=metadata,
+        )
+        self.index.append(entry)
+        self.messages_file.write(data)
+        self.offset += entry.length
+
+    def write_index(self):
+        with self.archive.open(RECORDING_INDEX_FILENAME, "w") as index_file:
+            data = [asdict(entry) for entry in self.index]
+            index_file.write(msgpack.packb(data))
+
+
 def record_messages(outfile: str | BytesIO, messages: Iterable[MessageEvent]):
-    with ZipFile(outfile, "w") as archive:
-        entries: list[RecordingIndexEntry] = []
-        try:
-            with archive.open(
-                RECORDING_MESSAGES_FILENAME, "w", force_zip64=True
-            ) as messages_file:
-                offset = 0
-                for event in messages:
-                    data = msgpack.packb(event.message)
-                    metadata = generate_metadata(event.message)
-                    metadata["timestamp"] = event.timestamp
-                    entry = RecordingIndexEntry(
-                        offset=offset,
-                        length=len(data),
-                        metadata=metadata,
-                    )
-                    entries.append(entry)
-                    messages_file.write(data)
-                    offset += entry.length
-        finally:
-            with archive.open(RECORDING_INDEX_FILENAME, "w") as index_file:
-                data = [asdict(entry) for entry in entries]
-                index_file.write(msgpack.packb(data))
+    with NanoverRecordingWriter(outfile) as writer:
+        for event in messages:
+            writer.write_message_event(event)
 
 
 def generate_metadata(message: dict[str, Any]) -> dict[str, Any]:
