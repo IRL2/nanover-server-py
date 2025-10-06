8fd1ec1ffb09cec59f257f891fa5fc270b091613
mark wonnacott <ragzouken@gmail.com>
2025-10-06 15:34:18 +0200

    cleanup

--- win_compile.ps1
+++ win_compile.ps1
@@ -1,4 +1,3 @@
-# Param statement must be first non-comment, non-blank line in the script
 param(
     [switch][alias("n")]$noedit = $false
 )
@@ -7,20 +6,16 @@ function announce {
     Write-Host $args[0] -ForegroundColor Green
 }
 
-$edit_option = ""
-
 if ($noedit)
 {
     announce "Installing nanover in non-edit mode."
+    python -m pip install "./python-libraries/nanover-server/[dev]"
 }
 else
 {
-    $edit_option = "-e"
-    Announce "Installing nanover-server-py in edit mode."
+    announce "Installing nanover-server-py in edit mode."
+    python -m pip install -e "./python-libraries/nanover-server/[dev]" --config-settings editable_mode=compat
 }
-
-announce "Installing the python packages"
-python -m pip install ${edit_option} "./python-libraries/nanover-server/[dev]" --config-settings editable_mode=compat
 
 python -c "import openmm"
 if ($LASTEXITCODE -ne 0)
