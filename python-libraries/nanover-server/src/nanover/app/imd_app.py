ffba416ee554032663b330a35fc81aa433149f65
Tim Neary <timdot10@gmail.com>
2025-10-08 14:56:54 +0100

    `NanoverImdApplication` now properly passes `port` parameter to the underlying WebSocket server.
    - Updated `__init__` to properly pass `port` to Websocket server.
    - Added utility property (funcs) to give standardised user access to Websocket ports
      (as these many not be what they specify)

--- python-libraries/nanover-server/src/nanover/app/imd_app.py
+++ python-libraries/nanover-server/src/nanover/app/imd_app.py
@@ -12,6 +12,7 @@ from nanover.app.app_server import NanoverApplicationServer
 from nanover.essd import DiscoveryServer
 from nanover.imd import ImdStateWrapper
 from .multiuser import add_multiuser_commands
+from ..websocket.server import DEFAULT_NANOVER_PORT
 
 
 class NanoverImdApplication(NanoverApplicationServer):
@@ -43,14 +44,16 @@ class NanoverImdApplication(NanoverApplicationServer):
         :param address: The address at which to bind the server to. If none given,
             the default address of
         :param port: Optional port on which to run the NanoVer server. If none given,
-            default port will be used.
+            will attempt to use the default port (38801) else a random port will be used.
         :param ssl: Optional SSLContext that if provided is used to host an additional
             secure websocket server on the WSS protocol.
         :return: An instantiation of a basic NanoVer server, registered with an
             ESSD discovery server.
         """
+        port = DEFAULT_NANOVER_PORT if port is None else port
+
         app_server = super().basic_server(name=name, address=address)
-        app_server.serve_websocket(ssl=ssl)
+        app_server.serve_websocket(ssl=ssl, port=port)
         return app_server
 
     def __init__(
@@ -64,15 +67,28 @@ class NanoverImdApplication(NanoverApplicationServer):
         self._imd_state = ImdStateWrapper(self._state_service.state_dictionary)
         add_multiuser_commands(self)
 
+    @property
+    def port(self) -> int | None:
+        """
+        Returns first available Websocket port. 
+        Insecure port will be returned if it exists else reports same as `secure_port`.
+        """
+        return self._server_ws.ws_port if self._server_ws is not None else self._server_ws.wss_port
+
+    @property
+    def secure_port(self) -> int | None:
+        """Returns SSL wrapped Websocket port, if available."""
+        return self._server_ws.wss_port if self._server_ws is not None else None
+
     def close(self):
         if self._server_ws is not None:
             self._server_ws.close()
         super().close()
 
-    def serve_websocket(self, *, insecure=True, ssl: SSLContext | None = None):
+    def serve_websocket(self, *, insecure=True, ssl: SSLContext | None = None, port: int = 0):
         from nanover.websocket.server import WebSocketServer
 
-        self._server_ws = WebSocketServer.basic_server(self, insecure=insecure, ssl=ssl)
+        self._server_ws = WebSocketServer.basic_server(self, insecure=insecure, ssl=ssl, port=port)
         return self._server_ws
 
     @property
