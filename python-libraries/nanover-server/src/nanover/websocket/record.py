622cb26a12cc6f0ca972b3c28751fae2cfdc76f9
mark wonnacott <ragzouken@gmail.com>
2025-10-10 16:42:17 +0200

    docstrings for websocket recording + cleanup

--- python-libraries/nanover-server/src/nanover/websocket/record.py
+++ python-libraries/nanover-server/src/nanover/websocket/record.py
@@ -1,9 +1,11 @@
 from concurrent.futures import ThreadPoolExecutor
+from contextlib import suppress
 from time import perf_counter_ns
 
 import msgpack
 from websockets import ConnectionClosed
-from websockets.sync.client import connect, ClientConnection
+from websockets.sync.client import connect
+from websockets.sync.connection import Connection
 
 from nanover.omni import OmniRunner
 from nanover.recording.reading import MessageEvent
@@ -40,20 +42,20 @@ def record_from_server(address, out_path):
     return executor, websocket
 
 
-def message_events_from_websocket(websocket: ClientConnection):
+def message_events_from_websocket(websocket: Connection):
+    """
+    Iterate the stream of incoming messages of a websocket connection and yield generic MessageEvents for use with the
+    recording functions.
+    """
     start_time = perf_counter_ns()
 
     def get_timestamp():
         return int((perf_counter_ns() - start_time) / 1000)
 
-    try:
+    with suppress(ConnectionClosed), websocket:
         for data in websocket:
             if isinstance(data, bytes):
                 yield MessageEvent(
                     timestamp=get_timestamp(),
                     message=msgpack.unpackb(data),
                 )
-    except ConnectionClosed:
-        pass
-    finally:
-        websocket.close()
