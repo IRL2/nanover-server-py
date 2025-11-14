00be5c045642abf4fb6e23e42d9bd80fc001b993
Tim Neary <timdot10@gmail.com>
2025-11-14 12:39:25 +0000

    Initial Simulation refactor.

--- python-libraries/nanover-server/src/nanover/core/simulation.py
+++ python-libraries/nanover-server/src/nanover/core/simulation.py
@@ -1,11 +1,56 @@
-from typing import Protocol
+from abc import abstractmethod
+from typing import Protocol, TypeVar
+
 from . import AppServer
+from nanover.trajectory import FrameData
 
 
-class Simulation(Protocol):
+S = TypeVar("S")
+
+
+class Simulation(Protocol[S]):
     name: str
+    app_server: AppServer
 
     def load(self): ...
-    def reset(self, app_server: AppServer): ...
-    def advance_by_one_step(self): ...
-    def advance_by_seconds(self, dt: float): ...
+    def reset(self, app_server: AppServer): ...  # Change to `reset(self)` ?
+
+    @abstractmethod
+    def make_topology_frame(self) -> FrameData: ...
+
+    @abstractmethod
+    def make_regular_frame(self) -> FrameData: ...
+
+    @abstractmethod
+    def advance_by_one_step(self):
+        # Should modify behaviour of `_advance_to_next_report` instead i.e. so moves by steps than sec.
+        ...
+
+    @abstractmethod
+    def advance_by_seconds(self, dt: float):
+        # Should modify behaviour of `_advance_to_next_report` instead i.e. so moves by sec rather than step.
+        ...
+
+    @abstractmethod
+    def _advance_to_next_report(self) -> S:
+        # Unified method to get next relevant simulation object
+        # e.g. return self.simulation.step() for openmm simulation
+        ...
+
+    @abstractmethod
+    def _frame_from_simulation(self, current_timepoint: S) -> FrameData:
+        """Constructs a frame from the current simulation timepoint."""
+        ...
+
+    @abstractmethod
+    def _cleanup(self, most_recent: FrameData | None = None) -> None:
+        """Cleans up any parameters/settings used in stepping to the next timepoint."""
+        ...
+
+    def send_next_frame(self) -> None:
+        """Advance the underlying simulation to the next frame and send it over the network."""
+        current_timestep = self._advance_to_next_report()
+        current_frame = self._frame_from_simulation(current_timestep)
+
+        self.app_server.frame_publisher.send_frame(current_frame)
+        self._cleanup(current_frame)
