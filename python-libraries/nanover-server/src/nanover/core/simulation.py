f3d3bf5cfb2f273d07b9dde8de6589fbfac2f1b3
Tim Neary <timdot10@gmail.com>
2025-11-19 13:52:26 +0000

    Added ReportableSimulation prototype example.

--- python-libraries/nanover-server/src/nanover/core/simulation.py
+++ python-libraries/nanover-server/src/nanover/core/simulation.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from typing import Protocol, TypeVar
+from typing import Protocol, TypeVar, Callable
 
 from . import AppServer
 from nanover.trajectory import FrameData
@@ -10,7 +10,6 @@ S = TypeVar("S")
 
 class Simulation(Protocol[S]):
     name: str
-    app_server: AppServer
 
     def load(self): ...
     def reset(self, app_server: AppServer): ...  # Change to `reset(self)` ?
@@ -34,7 +33,7 @@ class Simulation(Protocol[S]):
     @abstractmethod
     def _advance_to_next_report(self) -> S:
         # Unified method to get next relevant simulation object
-        # e.g. return self.simulation.step() for openmm simulation
+        # e.g. return self.simulation after .step() for openmm simulation
         ...
 
     @abstractmethod
@@ -43,14 +42,41 @@ class Simulation(Protocol[S]):
         ...
 
     @abstractmethod
+    def _send_frame(self, frame: FrameData) -> None:
+        # Sends frame via frame_publisher. Requred to maintain no AppServer member.
+        ...
+
+    @abstractmethod
     def _cleanup(self, most_recent: FrameData | None = None) -> None:
         """Cleans up any parameters/settings used in stepping to the next timepoint."""
         ...
 
-    def send_next_frame(self) -> None:
+    def advance_to_next_report(self) -> None:
         """Advance the underlying simulation to the next frame and send it over the network."""
         current_timestep = self._advance_to_next_report()
         current_frame = self._frame_from_simulation(current_timestep)
 
-        self.app_server.frame_publisher.send_frame(current_frame)
+        self._send_frame(current_frame)
+        self._cleanup(current_frame)
+
+
+class ReportableSimulation(Simulation):
+    _measurements = list[float]
+    measure_callbacks: set[Callable[[FrameData], float] | Callable[[S], float]]
+
+    def add_measurement(
+        self, callback: Callable[[S], float] | Callable[[FrameData], float]
+    ) -> None: ...
+
+    def _process_measurements(self, current_state: S) -> None:
+        _measurements = [func(current_state) for func in self.measure_callbacks]
+        # Add new measurement data to frame
+        # _measurements.add_to_frame(frame)
+
+    def advance_to_next_report(self):
+        current_timestep = self._advance_to_next_report()
+        current_frame = self._frame_from_simulation(current_timestep)
+        # self._process_measurements(current_timestep).add_to_frame(current_frame)
+
+        self._send_frame(current_frame)
         self._cleanup(current_frame)
