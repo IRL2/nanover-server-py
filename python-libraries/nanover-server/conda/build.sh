7fef101d20c0db97a7c7863c23b4f2aa9c1283aa
candle <ragzouken@gmail.com>
2025-12-22 14:58:31 +0100

    can package use uv?

--- python-libraries/nanover-server/conda/build.sh
+++ python-libraries/nanover-server/conda/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
+$PYTHON -m pip install uv
+$PYTHON -m uv pip install . --no-deps --ignore-installed --no-cache-dir -vvv
