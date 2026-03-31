b0fd3ea9741fd1d23697eb0f5bfc8df92d4e2359
TENeary <tim.neary@bristol.ac.uk>
2026-03-31 14:28:04 +0100

    Added tests to check simulations can be properly loaded from cmdline.

--- python-libraries/nanover-server/tests/test_cli/test_all.py
+++ python-libraries/nanover-server/tests/test_cli/test_all.py
@@ -1,5 +1,8 @@
 import subprocess
+import argparse
 from pathlib import Path
+
+from nanover.app.cli.server_cli import initialise_runner, handle_user_arguments
 
 import pytest
 
@@ -37,3 +40,66 @@ def test_split_recording(tmp_path_factory):
         ).returncode
         == 0
     )
+
+
+def test_load_mda_structure():
+    """Check that static structures can be loaded from commandline."""
+    pdb_path = Path(__file__).parent.parent / "test_mdanalysis/17-ala.pdb"
+    assert pdb_path.exists(), "Path is incorrect, this file should exist."
+
+    args = handle_user_arguments(args=["--mda", str(pdb_path)])
+
+    print(args)
+    with initialise_runner(args) as runner:
+        assert len(runner.simulations) == 1
+        runner.close()
+
+
+def test_load_mda_trajectory():
+    """Check that trajectories can be loaded from commandline."""
+    topology_path = (
+        Path(__file__).parent.parent.parent.parent.parent
+        / "tutorials/mdanalysis/files/3TI6_ose_wt.pdb"
+    )
+    trajectory_path = topology_path.with_name("ose_wt.dcd")
+    assert topology_path.exists(), "Path is incorrect, this file should exist."
+    assert trajectory_path.exists(), "Path is incorrect, this file should exist."
+
+    args = handle_user_arguments(
+        args=["--mda", str(topology_path), "--mda-traj", str(trajectory_path)]
+    )
+
+    with initialise_runner(args) as runner:
+        assert len(runner.simulations) == 1
+        runner.close()
+
+
+def test_load_multiple_mda_trajectories():
+    """Checks that mixed sets of trajectories and static structures can be loaded."""
+    pdb_path = Path(__file__).parent.parent / "test_mdanalysis/17-ala.pdb"
+    topology_path = (
+        Path(__file__).parent.parent.parent.parent.parent
+        / "tutorials/mdanalysis/files/3TI6_ose_wt.pdb"
+    )
+    trajectory_path = topology_path.with_name("ose_wt.dcd")
+
+    assert topology_path.exists(), "Path is incorrect, this file should exist."
+    assert trajectory_path.exists(), "Path is incorrect, this file should exist."
+    assert pdb_path.exists(), "Path is incorrect, this file should exist."
+
+    args = handle_user_arguments(
+        args=[
+            "--mda",
+            str(topology_path),
+            "--mda-traj",
+            str(trajectory_path),
+            "--mda",
+            str(pdb_path),
+        ]
+    )
+
+    with initialise_runner(args) as runner:
+        assert len(runner.simulations) == 2
+        assert runner.simulations[0].name == str(topology_path)
+        assert runner.simulations[1].name == str(pdb_path)
+        runner.close()
