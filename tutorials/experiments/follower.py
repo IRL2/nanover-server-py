09dd6f6db87eb6dc5c15c49c817932ab062bb27a
mark wonnacott <ragzouken@gmail.com>
2026-05-27 16:44:29 +0200

    fixes

--- tutorials/experiments/follower.py
+++ tutorials/experiments/follower.py
@@ -18,14 +18,15 @@ class Path:
         self.lengths: list[float] = []
 
     def add_point(self, point: npt.NDArray[np.float32]):
-        self.lengths.append(np.linalg.norm(point - self.points[-1], axis=0))
+        if self.points:
+            self.lengths.append(np.linalg.norm(point - self.points[-1], axis=0))
         self.points.append(point)
 
     def get_point_at_distance(self, distance: float) -> npt.NDArray[np.float32] | None:
         for i, length in enumerate(self.lengths):
             if length > distance:
                 u = distance / length
-                p =  self.points[i] + (self.points[i+1] - self.points[i]) * u
+                p = self.points[i] + (self.points[i + 1] - self.points[i]) * u
                 return p
             distance -= length
         return None
@@ -34,6 +35,7 @@ class Path:
 from dataclasses import dataclass, field
 from concurrent.futures import ThreadPoolExecutor
 from nanover.utilities.cli import CancellationToken
+
 
 @dataclass(kw_only=True)
 class Group:
@@ -52,32 +54,37 @@ class Follower:
         self._cancellation = CancellationToken()
         self._task = None
 
-    def start(self, groups: list[Group]):
+    def start(self, groups: list[Group], speed: float):
         if self._task is not None:
             return
 
         publisher = self._runner.app_server.frame_publisher
         imd = self._runner.app_server.imd
-        stream = publisher.subscribe_latest_frames(frame_interval=0, cancellation=self._cancellation)
+        stream = publisher.subscribe_latest_frames(
+            frame_interval=0, cancellation=self._cancellation
+        )
 
         def run():
             distance = 0
 
             for frame in stream:
-                distance += .01
+                distance += speed
 
                 for i, group in enumerate(groups):
                     key = f"interaction.REPLAYER.{i}"
-                    #centroid = np.average(frame.particle_positions[group.particles], axis=0)
+                    # centroid = np.average(frame.particle_positions[group.particles], axis=0)
                     target = group.path.get_point_at_distance(distance)
 
                     if target is not None:
-                        imd.insert_interaction(key, ParticleInteraction(
-                            particles=group.particles,
-                            position=list(target),
-                            interaction_type="spring",
-                            scale=500,
-                        ))
+                        imd.insert_interaction(
+                            key,
+                            ParticleInteraction(
+                                particles=group.particles,
+                                position=list(target),
+                                interaction_type="spring",
+                                scale=500,
+                            ),
+                        )
                     else:
                         imd.remove_interaction(key)
 
