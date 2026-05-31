6639313304c6868e534e3f453cca3cb12056b607
candle <ragzouken@gmail.com>
2026-05-31 15:25:26 +0200

    cleanup

--- tutorials/experiments/smearmd.py
+++ tutorials/experiments/smearmd.py
@@ -7,12 +7,14 @@ import numpy.typing as npt
 
 from nanover.app import OmniRunner
 from nanover.imd import ParticleInteraction
+from nanover.trajectory import FrameData
 from nanover.utilities.cli import CancellationToken
 
 
 @dataclass(kw_only=True, eq=False)
 class TargetGroup:
     particles: list[int] = field(default_factory=list)
+    position: npt.NDArray[np.float32]
     centroid: npt.NDArray[np.float32]
 
 
@@ -23,6 +25,17 @@ class KeyFrame:
 
     def __post_init__(self):
         self.centroids = np.array([target.centroid for target in self.targets])
+
+
+def fit_keyframe_to_frame(keyframe: KeyFrame, frame: FrameData):
+    current = np.array([
+        np.mean(frame.particle_positions[target.particles], axis=0)
+        for target in keyframe.targets
+    ])
+
+    targets = fit_template_points_to_observed(keyframe.centroids, current)
+
+    return current, targets
 
 
 class SmearAgent:
@@ -36,13 +49,47 @@ class SmearAgent:
         self._cancellation = CancellationToken()
         self._task = None
         self._interactions: set[str] = set()
+        self.speed = .1
 
-    def start(self, keyframe: KeyFrame, speed: float, output):
+    def update_interactions(self, keyframe, frame):
+        imd = self._runner.app_server.imd
+
+        # fit keyframe targets to actual positions
+        prev_centroids, next_centroids = fit_keyframe_to_frame(keyframe, frame)
+
+        # find necessary motions
+        deltas = next_centroids - prev_centroids
+
+        # cap motions by speed
+        lengths = np.linalg.norm(deltas, axis=1).reshape(-1, 1)
+        cappeds = deltas / lengths
+        np.clip(lengths, max=self.speed, out=lengths)
+        cappeds *= lengths
+
+        # determine final interaction positions
+        target_centroids = prev_centroids + cappeds
+
+        for i, target in enumerate(keyframe.targets):
+            key = f"interaction.REPLAYER.{i}"
+            self._interactions.add(key)
+
+            if lengths[i] < 0.0001:
+                continue
+
+            interaction = ParticleInteraction(
+                particles=target.particles,
+                position=list(target_centroids[i]),
+                interaction_type="spring",
+                scale=1000,
+                max_force=50,
+            )
+            imd.insert_interaction(key, interaction)
+
+    def start(self, keyframe: KeyFrame, output):
         if self._task is not None:
             return
 
         publisher = self._runner.app_server.frame_publisher
-        imd = self._runner.app_server.imd
         stream = publisher.subscribe_latest_frames(
             frame_interval=0, cancellation=self._cancellation
         )
@@ -50,42 +97,7 @@ class SmearAgent:
         def run():
             try:
                 for frame in stream:
-                    prev_centroids = np.array([
-                        np.mean(frame.particle_positions[target.particles], axis=0)
-                        for target in keyframe.targets
-                    ])
-
-                    # fit keyframe targets to actual positions
-                    next_centroids = fit_template_points_to_observed(keyframe.centroids, prev_centroids)
-
-                    # find necessary motions
-                    deltas = next_centroids - prev_centroids
-                    error = np.sum(deltas)
-
-                    # cap motions by speed
-                    lengths = np.linalg.norm(deltas, axis=1).reshape(-1, 1)
-                    cappeds = deltas / lengths
-                    np.clip(lengths, max=speed, out=lengths)
-                    cappeds *= lengths
-
-                    # determine final interaction positions
-                    target_centroids = prev_centroids + cappeds
-
-                    for i, target in enumerate(keyframe.targets):
-                        key = f"interaction.REPLAYER.{i}"
-                        self._interactions.add(key)
-
-                        if lengths[i] > 0.0001:
-                            imd.insert_interaction(
-                                key,
-                                ParticleInteraction(
-                                    particles=target.particles,
-                                    position=list(target_centroids[i]),
-                                    interaction_type="spring",
-                                    scale=500,
-                                    max_force=500,
-                                ),
-                            )
+                    self.update_interactions(keyframe, frame)
             except Exception as e:
                 with output:
                     print(traceback.print_exc())
@@ -99,29 +111,32 @@ class SmearAgent:
             self._runner.app_server.imd.remove_interaction(key)
 
 
-def fit_template_points_to_observed(template_points: npt.NDArray, observed_points: npt.NDArray):
-    # minimise (Rp+t - q)**2 -- fit template points onto observed points
-    p = template_points.transpose()
-    q = observed_points.transpose()
+def fit_template_points_to_observed(
+    template_points: npt.NDArray,
+    observed_points: npt.NDArray,
+):
+    # https://web.stanford.edu/class/cs273/refs/umeyama.pdf
+    # https://doi.org/10.1109/34.88573
+    p = template_points.T
+    q = observed_points.T
 
     # centroids
     cen_P = np.mean(p, axis=1).reshape(-1, 1)
     cen_Q = np.mean(q, axis=1).reshape(-1, 1)
 
-    # positions relative to centroids ("centered vectors")
+    # centered vectors
     X = p - cen_P
     Y = q - cen_Q
 
-    # covariance matrix
-    S = X @ Y.T
-    U, sigma, Vt = np.linalg.svd(S)
+    # svd
+    U, sigma, Vt = np.linalg.svd(X @ Y.T)
 
-    # diagonal matrix
-    d = np.eye(Vt.T.shape[1])
+    # reflection correction
+    d = np.identity(Vt.T.shape[1])
     d[-1, -1] = np.linalg.det(Vt.T @ U.T)
 
     # rotation and translation
     R = Vt.T @ d @ U.T
     t = cen_Q - R @ cen_P
 
-    return (R @ p + t).transpose()
+    return (R @ p + t).T
