1eddd2136a17d4ef39a2a09d86b288c4bc65e0b5
mark wonnacott <ragzouken@gmail.com>
2026-06-18 15:51:19 +0200

    fixes + better page

--- python-libraries/nanover-server/src/nanover/websocket/landing.py
+++ python-libraries/nanover-server/src/nanover/websocket/landing.py
@@ -2,11 +2,15 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from ssl import SSLContext
 
 
+LANDING_HTML = """<dialog open style="inset: 0;"><button onclick="window.history.back(); window.close()" style="font-size: 10rem;">DONE</button></dialog>"""
+
+
 class StaticHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200, "HELLO")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write("HELLO".encode("UTF-8"))
+        self.wfile.write(LANDING_HTML.encode("UTF-8"))
 
 
 def make_landing_page_server(
