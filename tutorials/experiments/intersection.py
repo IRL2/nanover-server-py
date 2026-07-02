2bead440de1ec7d82d8a1a5dd28892f4d2f6d573
mark wonnacott <ragzouken@gmail.com>
2026-07-02 13:13:01 +0200

    spline edit test

--- tutorials/experiments/intersection.py
+++ tutorials/experiments/intersection.py
@@ -2,6 +2,5 @@ import numpy as np
 
 
 def intersects_sphere_line(center: list[float], radius: float, positions: list):
-    deltas = np.subtract(center, positions)
-    length2 = np.sum(np.square(deltas), axis=1)
+    length2 = np.sum(np.subtract(center, positions) ** 2, axis=1)
     return np.any(length2 <= radius * radius)
