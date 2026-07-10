0d27f34ae6945f95233ff613b7ab18619b2200eb
mark wonnacott <ragzouken@gmail.com>
2026-07-10 17:46:05 +0200

    fix: hugely optimise line closest_point_on_polyline and improve return value structure (#589)

--- tutorials/utilities/intersection.py
+++ tutorials/utilities/intersection.py
@@ -1,38 +1,40 @@
-import math
+from dataclasses import dataclass
 import numpy as np
 
 
-def closest_point_on_segment(p1, p2, pt):
-    p1 = np.array(p1)
-    p2 = np.array(p2)
-    pt = np.array(pt)
-
-    p2p1 = p2 - p1
-    p2p1dot = p2p1.dot(p2p1)
-
-    if p2p1dot == 0:
-        return p1, np.linalg.norm(p1 - pt)
-
-    ptp1 = pt - p1
-
-    t = np.dot(ptp1, p2p1) / p2p1dot
-
-    if t < 0:
-        closest = p1
-    elif t > 1:
-        closest = p2
-    else:
-        closest = p1 + t * p2p1
-
-    return closest, np.linalg.norm(closest - pt)
+@dataclass(kw_only=True)
+class ClosestPointResult:
+    point: np.ndarray
+    distance: float
+    index: int
+    t: float
 
 
 def closest_point_on_polyline(points: list, target):
-    best = (points[0], math.inf, 0)
+    # each point to next delta
+    p2p1 = np.diff(points, axis=0)
+    # each point to next length squared
+    p2p1dot = (p2p1 * p2p1).sum(1)
+    # each point to target delta
+    ptp1 = np.subtract(target, points)[:-1]
+    # percent along each segment the target is
+    t = (ptp1 * p2p1).sum(1) / p2p1dot
+    # clamp between 0,1
+    np.clip(t, 0, 1, out=t)
 
-    for i, (p1, p2) in enumerate(zip(points, points[1:])):
-        point, dist = closest_point_on_segment(p1, p2, target)
-        if dist < best[1]:
-            best = (point, dist, i)
+    # each segment closest point (p1 + t * p2p1)
+    pc = np.add(points[:-1], t.reshape(-1, 1) * p2p1)
+    # each closest point to target delta
+    ptpc = np.subtract(target, pc)
+    # each closest point to target length squared
+    ptpcdot = (ptpc * ptpc).sum(1)
 
-    return best
+    # index of closest point to target minimum length squared
+    index = np.argmin(ptpcdot)
+
+    return ClosestPointResult(
+        point=points[index],
+        distance=np.sqrt(ptpcdot[index]),
+        index=index,
+        t=t,
+    )
