31e981b8d604ff8c14490afc9301cc763ea6f59d
candle <ragzouken@gmail.com>
2026-07-13 22:35:08 +0200

    tests for alignment

--- python-libraries/nanover-server/src/nanover/utilities/transforms.py
+++ python-libraries/nanover-server/src/nanover/utilities/transforms.py
@@ -48,3 +48,43 @@ class Transform:
 
 def _transform_vec3(matrix, vector):
     return (matrix @ np.array([*vector[:3], 1]).reshape(4, 1)).reshape(4)[:3]
+
+
+def find_transformation_between_points(
+    start_points: npt.NDArray,
+    final_points: npt.NDArray,
+):
+    """
+    Return the 4x4 transformation matrix that best maps the start points to the final points.
+    """
+    # https://web.stanford.edu/class/cs273/refs/umeyama.pdf
+    # https://doi.org/10.1109/34.88573
+    p = start_points.T
+    q = final_points.T
+
+    # centroids
+    cen_P = np.mean(p, axis=1).reshape(-1, 1)
+    cen_Q = np.mean(q, axis=1).reshape(-1, 1)
+
+    # centered vectors
+    X = p - cen_P
+    Y = q - cen_Q
+
+    # svd
+    U, sigma, Vt = np.linalg.svd(X @ Y.T)
+
+    # reflection correction
+    d = np.identity(Vt.T.shape[1])
+    d[-1, -1] = np.linalg.det(Vt.T @ U.T)
+
+    # rotation and translation
+    R = Vt.T @ d @ U.T
+    t = cen_Q - R @ cen_P
+
+    # convert to TRS matrix4x4
+    transform = np.identity(4)
+    transform[:3, 3] = t.reshape(-1)
+    transform[:3, :3] = R
+
+    return transform
+
