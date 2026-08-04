a5fe3c1f0a46cbf861c40a955bd6897baac61244
mark wonnacott <ragzouken@gmail.com>
2026-08-04 11:21:26 +0200

    rotation only handle

--- python-libraries/nanover-server/src/nanover/jupyter/transform_handles.py
+++ python-libraries/nanover-server/src/nanover/jupyter/transform_handles.py
@@ -7,6 +7,7 @@ from . import Mode, NanoverJupyterUtilities
 def use_transform_handles(utilities: NanoverJupyterUtilities):
     cursor_grabbed_handle: dict[str, dict] = {}
     cursor_grabbed_matrix: dict = {}
+    cursor_grabbed_original: dict = {}
 
     def intersect_all_handles(point):
         for key, handle in utilities.handles.all_prefixed_items():
@@ -19,11 +20,17 @@ def use_transform_handles(utilities: NanoverJupyterUtilities):
 
     def cursor_in_object_parent_matrix(cursor: dict, object: str):
         object_parent = utilities.transforms.get_parent(object, default="root")
-        cursor_to_root = Transform.from_state_cursor(cursor).local_to_parent_matrix
         parent_to_root = utilities.transforms.fetch_transform_root(
             object_parent
         ).local_to_parent_matrix
+        cursor_to_root = Transform.from_state_cursor(cursor).local_to_parent_matrix
         return np.linalg.inv(parent_to_root) @ cursor_to_root
+
+    def filter_matrix(cursor, target, handle: dict):
+        cursor = cursor.copy()
+        if not handle.get("translate", False):
+            cursor[:3, 3] = target[:3, 3]
+        return cursor
 
     class MoveObjectMode(Mode):
         def on_button_pressed(self, *, key: str, cursor: dict, button: str):
@@ -47,12 +54,14 @@ def use_transform_handles(utilities: NanoverJupyterUtilities):
 
                 cursor_grabbed_handle[key] = hovered
                 cursor_grabbed_matrix[key] = offset_matrix
+                cursor_grabbed_original[key] = object_in_parent
 
         def on_button_released(self, *, key: str, cursor: dict, button: str):
             # release grabbed
             if button == "primary":
                 cursor_grabbed_handle.pop(key, None)
                 cursor_grabbed_matrix.pop(key, None)
+                cursor_grabbed_original.pop(key, None)
 
         def on_cursor_updated(self, *, key: str, cursor: dict):
             # if this cursor has grabbed an object, update the object pose from cursor pose
@@ -70,6 +79,10 @@ def use_transform_handles(utilities: NanoverJupyterUtilities):
                 # object matrix relative to object parent
                 object_in_parent = cursor_in_parent @ offset_matrix
 
+                object_in_parent = filter_matrix(
+                    object_in_parent, cursor_grabbed_original.get(key), grabbed
+                )
+
                 utilities.transforms.update_transform(
                     transform,
                     transform=Transform.from_local_to_parent_matrix(object_in_parent),
@@ -77,7 +90,7 @@ def use_transform_handles(utilities: NanoverJupyterUtilities):
                 )
 
             # show/remove hover graphic is this cursor hovers something
-            hovered = intersect_all_handles(cursor["position"])
+            hovered = grabbed or intersect_all_handles(cursor["position"])
             if hovered is None:
                 utilities.objects.update_shape(f"hovered.{key}")
             else:
@@ -86,7 +99,7 @@ def use_transform_handles(utilities: NanoverJupyterUtilities):
                     f"hovered.{key}",
                     position=center,
                     color=[1.0, 1.0, 0.0, 0.5],
-                    size=radius,
+                    size=radius * 2,
                     parent=hovered["parent"],
                 )
 
