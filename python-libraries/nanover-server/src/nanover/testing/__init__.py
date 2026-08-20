2f6b60b6660229c83fd792d1126c78e5db7806ca
mark wonnacott <ragzouken@gmail.com>
2026-08-20 16:01:16 +0200

    chore: switch to non-pyproject config for top level config. put assert rewriting in package code (#627)

--- python-libraries/nanover-server/src/nanover/testing/__init__.py
+++ python-libraries/nanover-server/src/nanover/testing/__init__.py
@@ -1,3 +1,10 @@
+try:
+    import pytest
+
+    pytest.register_assert_rewrite("nanover.testing.asserts")
+except ImportError:
+    pass
+
 from .asserts import (
     assert_equal_soon as assert_equal_soon,
     assert_in_soon as assert_in_soon,
