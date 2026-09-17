70db0ae7b21e427069bb861f31d66f7178a1df0f
Harry Stroud <75842225+hjstroud@users.noreply.github.com>
2026-09-17 14:22:52 +0200

    Fix type error

--- src/nanover/imd/imd_force.py
+++ src/nanover/imd/imd_force.py
@@ -297,7 +297,7 @@ def calculate_constant_force(
 
     if distance_magnitude > 0:
         force = distance_vector / distance_magnitude
-        energy = distance_magnitude
+        energy = float(distance_magnitude)
     else:
         force = distance_vector * 0
         energy = 0
